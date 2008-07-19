5e4cde59ef0961a7fdb39b70683d9a66214fd00e
Chad Woolley <thewoolleyman@gmail.com>
2008-07-18 23:11:21 -0700

    put chads emails in cruise_config.rb for now

--- ci/cruise_config.rb
+++ ci/cruise_config.rb
@@ -1,5 +1,5 @@
 Project.configure do |project|
  project.build_command = 'ruby ../rails_build.rb'
- project.email_notifier.emails = ['rubyonrails-core@googlegroups.com']
- project.email_notifier.from = 'alexey.verkhovsky@gmail.com'
+ project.email_notifier.emails = ['thewoolleyman@gmail.com']
+ project.email_notifier.from = 'thewoolleyman+railsci@gmail.com'
 end
