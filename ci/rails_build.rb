659f766913d82a99d1c24b5f872ab2da52ba3b65
Chad Woolley <thewoolleyman@gmail.com>
2008-07-18 23:00:41 -0700

    ci server setup notes, and ccrb config files from Alexey's previous CI setup

---
+++ ci/rails_build.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+include FileUtils
+cd "#{File.dirname(__FILE__)}/work"
+
+puts "[CruiseControl] Rails build"
+
+build_results = {}
+
+cd 'activesupport' do
+  puts
+  puts "[CruiseControl] Building ActiveSupport"
+  puts
+  build_results[:activesupport] = system 'rake'
+end
+
+cd 'activerecord' do
+  puts
+  puts "[CruiseControl] Building ActiveRecord with MySQL"
+  puts
+  build_results[:activerecord_mysql] = system 'rake test_mysql'
+end
+
+# This section is commented out because Postgres started segfaulting on the Mac Mini.
+# Uncomment after moving the site
+cd 'activerecord' do
+  puts
+  puts "[CruiseControl] Building ActiveRecord with PostgreSQL"
+  puts
+  build_results[:activerecord_postgresql8] = system 'rake test_postgresql'
+end
+
+# Commented out until I install SQLite 2 on the new build box
+#cd 'activerecord' do
+#  puts
+#  puts "[CruiseControl] Building ActiveRecord with SQLite 2"
+#  puts
+#  build_results[:activerecord_sqlite] = system 'rake test_sqlite'
+#end
+
+cd 'activerecord' do
+  puts
+  puts "[CruiseControl] Building ActiveRecord with SQLite 3"
+  puts
+  build_results[:activerecord_sqlite3] = system 'rake test_sqlite3'
+end
+
+cd 'activeresource' do
+  puts
+  puts "[CruiseControl] Building ActiveResource"
+  puts
+  build_results[:activeresource] = system 'rake'
+end
+
+cd 'actionpack' do
+  puts
+  puts "[CruiseControl] Building ActionPack"
+  puts
+  build_results[:actionpack] = system 'rake'
+end
+
+cd 'actionmailer' do
+  puts
+  puts "[CruiseControl] Building ActionMailer"
+  puts
+  build_results[:actionmailer] = system 'rake'
+end
+
+
+puts
+puts "[CruiseControl] Build environment:"
+puts "[CruiseControl]   #{`uname -a`}"
+puts "[CruiseControl]   #{`ruby -v`}"
+puts "[CruiseControl]   #{`/usr/bin/mysql --version`}"
+puts "[CruiseControl]   #{`/usr/bin/postgres --version`}"
+puts "[CruiseControl]   SQLite3: #{`/usr/bin/sqlite3 -version`}"
+puts "[CruiseControl]   Local gems: #{`gem list --local`}"
+puts
+
+failures = build_results.select { |key, value| value == false }
+
+if failures.empty?
+  puts
+  puts "[CruiseControl] Rails build finished sucessfully"
+  exit(0)
+else
+  puts
+  puts "[CruiseControl] Rails build FAILED"
+  puts "[CruiseControl] Failed components: #{failures.map { |component| component.first }.join(', ')}"
+  exit(-1)
+end
+
