07726f3c21fad4479eae5b4ebf75a7fce598fe73
Chad Woolley <thewoolleyman@gmail.com>
2008-07-19 11:42:44 -0700

    reorganize ci stuff to prepare for merge to rails mainline

--- ci/cruise_config.rb
+++
@@ -1,5 +0,0 @@
-Project.configure do |project|
- project.build_command = 'ruby ../rails_build.rb'
- project.email_notifier.emails = ['thewoolleyman@gmail.com']
- project.email_notifier.from = 'thewoolleyman+railsci@gmail.com'
-end
