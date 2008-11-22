b73ef4fa3b74ffaabd93dd97dbf72404ec486a91
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-11-22 22:02:16 +0100

    Cleaned up the script/plugin install command.

--- railties/lib/commands/plugin.rb
+++ railties/lib/commands/plugin.rb
@@ -750,7 +750,6 @@ module Commands
   class Install
     def initialize(base_command)
       @base_command = base_command
-      @method = :http
       @options = { :quiet => false, :revision => nil, :force => false }
     end
     
@@ -762,22 +761,13 @@ module Commands
         o.separator   ""
         o.separator   "Options:"
         o.on(         "-m", "--method=METHOD", String,
-                      "Use an alternative install method.") { |method| @secondary_method = method.to_sym }
-        o.on(         "-x", "--externals", 
-                      "Use svn:externals to grab the plugin.", 
-                      "Enables plugin updates and plugin versioning.") { |v| @method = :externals }
-        o.on(         "-o", "--checkout",
-                      "Use svn checkout to grab the plugin.",
-                      "Enables updating but does not add a svn:externals entry.") { |v| @method = :checkout }
-        o.on(         "-e", "--export",
-                      "Use svn export to grab the plugin.",
-                      "Exports the plugin, allowing you to check it into your local repository. Does not enable updates, or add an svn:externals entry.") { |v| @method = :export }
+                      "Use an alternative install method.") { |method| @options[:method] = method.to_sym }
         o.on(         "-q", "--quiet",
                       "Suppresses the output from installation.",
                       "Ignored if -v is passed (./script/plugin -v install ...)") { |v| @options[:quiet] = true }
         o.on(         "-r REVISION", "--revision REVISION",
-                      "Checks out the given revision from subversion or git.",
-                      "Ignored if subversion/git is not used.") { |v| @options[:revision] = v }
+                      "Installs the given revision.",
+                      "Only works with plugin managers which support this.") { |v| @options[:revision] = v }
         o.on(         "-f", "--force",
                       "Reinstalls a plugin if it's already installed.") { |v| @options[:force] = true }
         o.separator   ""
@@ -786,30 +776,8 @@ module Commands
       end
     end
     
-    def determine_install_method
-      best = @base_command.environment.best_install_method
-      @method = :http if best == :http and @method == :export
-      case
-      when (best == :http and @method != :http)
-        msg = "Cannot install using subversion because `svn' cannot be found in your PATH"
-      when (best == :export and (@method != :export and @method != :http))
-        msg = "Cannot install using #{@method} because this project is not under subversion."
-      when (best != :externals and @method == :externals)
-        msg = "Cannot install using externals because vendor/plugins is not under subversion."
-      end
-      if msg
-        puts msg
-        exit 1
-      end
-      @method
-    end
-    
     def parse!(args)
       options.parse!(args)
-      environment = @base_command.environment
-      install_method = determine_install_method
-      puts "Plugins will be installed using #{install_method}" if $verbose
-      @options.merge!(:method => @secondary_method)
       args.each do |name|
         Rails::PluginManager.install(name, @options)
       end
