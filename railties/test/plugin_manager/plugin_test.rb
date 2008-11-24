58b2bf8dfc89ef824dcd99d4ddf0083bcfe659a0
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-11-24 12:45:06 +0100

    Improved the test coverage of the base Plugin class.

--- railties/test/plugin_manager/plugin_test.rb
+++ railties/test/plugin_manager/plugin_test.rb
@@ -7,16 +7,22 @@ include Rails::PluginManager
 
 module PluginManager
   class PluginTest < Test::Unit::TestCase
-    def setup
-      @plugin = Plugin.new(:uri => "http://example.com/acts_as_awesome")
-    end
-
     def test_plugin_should_have_an_uri
-      assert_equal "http://example.com/acts_as_awesome", @plugin.uri
+      assert_equal "http://example.com/acts_as_awesome", plugin.uri
     end
 
     def test_plugin_should_infer_a_name
-      assert_equal "acts_as_awesome", @plugin.name
+      assert_equal "acts_as_awesome", plugin.name
     end
+
+    def test_plugin_should_not_infer_name_if_provided_manually
+      assert_equal "acts_as_lame", plugin(:name => "acts_as_lame").name
+    end
+
+    protected
+
+      def plugin(options = {})
+        Plugin.new(options.merge(:uri => "http://example.com/acts_as_awesome"))
+      end
   end
 end
