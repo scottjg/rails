8c08341eb5fa24dde848e95ed96fb712da0ea909
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-11-24 15:52:02 +0100

    Removed PluginManager::Base and Helpers.

--- railties/lib/plugin_manager.rb
+++ railties/lib/plugin_manager.rb
@@ -2,12 +2,8 @@
 $:.unshift(File.dirname(__FILE__))
 $:.unshift(File.dirname(__FILE__) + "/../../activesupport/lib")
 
-require 'plugin_manager/helpers'
 require 'plugin_manager/commands'
 
 module Rails::PluginManager
   extend Rails::PluginManager::Commands
 end
-
-require 'plugin_manager/base'
-require 'plugin_manager/managers/git_plugin_manager'
