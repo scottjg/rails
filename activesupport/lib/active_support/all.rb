b69c92f2ee00d9e51cb2506cdab9165cff93bcf4
Michael S. Klishin <michael@novemberain.com>
2008-12-29 04:31:48 +0300

    Introduce active_support/all.rb that loads "everything", and move Rails packages to it.
    In the future each library will only load what it uses, but right now it needs a lot of
    investigation first. Builder for whatever reason cannot load after String core extensions,
    and so forth.
    
    So consider this intermediate step to AS modularity as a must.

---
+++ activesupport/lib/active_support/all.rb
@@ -0,0 +1,6 @@
+require 'active_support/vendor'
+require 'active_support/core_ext'
+require 'active_support/dependencies'
+require 'active_support/json'
+
+I18n.load_path << "#{File.dirname(__FILE__)}/locale/en.yml"
