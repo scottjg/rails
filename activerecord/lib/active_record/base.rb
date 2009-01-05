69cca0445cdfc9a88d10a8a6c6eabb84724f90a5
David Genord II <david@xspond.com>
2009-01-05 13:48:50 -0500

    Fixed a bug with the default select statement

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -1363,7 +1363,7 @@ module ActiveRecord #:nodoc:
 
         def construct_finder_sql(options)
           scope = scope(:find)
-          sql  = "SELECT #{(scope && scope[:select]) || options[:select] || (options[:joins] && quoted_table_name + '.*') || '*'} "
+          sql  = "SELECT #{(scope && scope[:select]) || options[:select] || ((options[:joins] || (scope && scope[:joins])) && quoted_table_name + '.*') || '*'} "
           sql << "FROM #{(scope && scope[:from]) || options[:from] || quoted_table_name} "
 
           add_joins!(sql, options, scope)
