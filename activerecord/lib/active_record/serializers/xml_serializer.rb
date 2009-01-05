c0817a0594df4ac9bc08d217e137c45faa422bbb
David Genord II <david@xspond.com>
2009-01-05 14:10:47 -0500

    xml_serializer improperly nuking methods included on to_xml includes

--- activerecord/lib/active_record/serializers/xml_serializer.rb
+++ activerecord/lib/active_record/serializers/xml_serializer.rb
@@ -207,8 +207,7 @@ module ActiveRecord #:nodoc:
             records.each do |record|
               record.to_xml opts.merge(
                 :root => association_name,
-                :type => (record.class.to_s.underscore == association_name ? nil : record.class.name),
-                :methods => []
+                :type => (record.class.to_s.underscore == association_name ? nil : record.class.name)
               )
             end
           end
