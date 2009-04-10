4fa15129c0b27302af3dc5a86539d25a2f011f2f
Chad Woolley <thewoolleyman@gmail.com>
2009-04-10 14:05:22 -0700

    Work on new automated CI setup instructions and scripts

--- ci/setup_rails_ci.sh
+++ ci/setup_rails_ci.sh
@@ -8,3 +8,10 @@ echo "  Creating ci user..."
 sudo useradd -s /bin/bash -m -p `mkpasswd -H md5 password` ci
 
 echo "  Giving ci user no-password sudo privileges"
+rm /tmp/sudoers.tmp
+sudo cp /etc/sudoers /etc/sudoers.bak
+cp /etc/sudoers /tmp/sudoers.tmp
+echo "ci      ALL=(ALL) NOPASSWD: ALL" >> /tmp/sudoers.tmp
+visudo -q -c -s -f /tmp/sudoers.tmp
+if [ ! $? = 0 ]; then echo "error editing sudoers file, run 'visudo -q -c -s -f /tmp/sudoers.tmp'" && exit; fi
+sudo cp /tmp/sudoers.tmp /etc/sudoers
