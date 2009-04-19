d35b375d899572915506357ddabe36bbe952c5b9
Chad Woolley <thewoolleyman@gmail.com>
2009-04-19 15:09:17 -0700

    rearranging and adding docs for planned implementation

---
+++ ci/bootstrap_rails_ci.rb
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Setting up Rails Continuous Integration server..."
+
+if [ -e /home/ci ]; then echo "ci user already exists.  To delete the ci user and start from scratch, type 'sudo deluser --remove-home ci'" && exit; fi
+  
+echo "  Creating ci user..."
+sudo useradd -s /bin/bash -m -p `mkpasswd -H md5 password` ci
+
+echo "  Giving ci user no-password sudo privileges"
+rm /tmp/sudoers.tmp
+sudo cp /etc/sudoers /etc/sudoers.bak
+cp /etc/sudoers /tmp/sudoers.tmp
+echo "ci      ALL=(ALL) NOPASSWD: ALL" >> /tmp/sudoers.tmp
+visudo -q -c -s -f /tmp/sudoers.tmp
+if [ ! $? = 0 ]; then echo "error editing sudoers file, run 'visudo -q -c -s -f /tmp/sudoers.tmp'" && exit; fi
+sudo cp /tmp/sudoers.tmp /etc/sudoers
+
+echo "  Downloading setup scripts..."
+sudo su - -c 'mkdir ~/cinabox' ci 
+sudo su - -c 'wget -O ~/cinabox/cinabox.tar.gz http://github.com/thewoolleyman/cinabox/blob/master/setup_rails_dependencies.rb?raw=true' ci
+sudo su - -c 'mkdir ~/railsci' ci 
+sudo su - -c 'wget -O ~/railsci/setup_rails_dependencies.rb http://github.com/rails/rails/blob/master/ci/setup_rails_ci.rb?raw=true' ci
