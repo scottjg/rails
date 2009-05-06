14c1c4393e3397151b577740c2b849fed0497dcc
Chad Woolley <thewoolleyman@gmail.com>
2009-05-06 01:07:52 -0700

    ci setup scripts - in progress

--- ci/bootstrap_rails_ci.sh
+++ ci/bootstrap_rails_ci.sh
@@ -18,3 +18,9 @@ sudo su - -c "tar --directory=/home/ci/cinabox --overwrite --strip-components=1 
 echo "  Downloading setup_rails_dependencies to /home/ci/railsci..."
 sudo su - -c "mkdir ~/railsci" ci
 sudo su - -c "wget -O ~/railsci/setup_rails_dependencies.rb $RAILS_GIT_DOWNLOAD_URL/ci/setup_rails_dependencies.rb" ci
+
+echo "  Download complete!  Please complete the remaining steps:"
+echo "  1. Log in as ci user: 'sudo su - ci'"
+echo "  2. Run bootstrap_ruby.sh: 'RUBY_VERSION=1.8.6-p287 ~/cinabox/bootstrap_ruby.sh'"
+echo "  3. Run setup_ci: 'ruby ~/cinabox/setup_ci.rb'"
+echo "  4. Run setup_rails_dependencies.rb: 'ruby ~/railsci/setup_rails_dependencies.rb'"
