3422b40961bcc29a27be7f1a7d22ad16b1914039
Chad Woolley <thewoolleyman@gmail.com>
2009-05-06 00:48:26 -0700

    ci setup scripts - in progress

--- ci/bootstrap_rails_ci.sh
+++ ci/bootstrap_rails_ci.sh
@@ -2,21 +2,9 @@
 
 echo "Setting up Rails Continuous Integration server..."
 
-# TODO: prompt to autodelete existing ci user
-if [ -e /home/ci ]; then echo "ci user already exists.  To delete the ci user and start from scratch, type 'sudo deluser --remove-home ci'" && exit; fi
-
-echo "  Creating ci user..."
-if [ -z $CI_PASSWORD ]; then read -p "    Please type password for ci user and press enter:" -s -a CI_PASSWORD; fi
-sudo useradd -s /bin/bash -m -p `mkpasswd -H md5 $CI_PASSWORD` ci
-
-echo "  Giving ci user no-password sudo privileges"
-rm /tmp/sudoers.tmp
-sudo cp /etc/sudoers /etc/sudoers.bak
-cp /etc/sudoers /tmp/sudoers.tmp
-echo "ci      ALL=(ALL) NOPASSWD: ALL" >> /tmp/sudoers.tmp
-visudo -q -c -s -f /tmp/sudoers.tmp
-if [ ! $? = 0 ]; then echo "error editing sudoers file" && exit; fi
-sudo cp /tmp/sudoers.tmp /etc/sudoers
+wget -O ~/create_ci_user.sh http://github.com/thewoolleyman/rails/raw/master/create_ci_user.sh
+chmod a+x ~/create_ci_user.sh
+~/create_ci_user.sh
 
 if [ -z $RAILS_GIT_DOWNLOAD_URL ]; then RAILS_GIT_DOWNLOAD_URL='http://github.com/thewoolleyman/rails/raw/master'; fi
 
@@ -24,9 +12,8 @@ echo "  Downloading cinabox..."
 sudo su - -c "mkdir ~/cinabox" ci
 sudo su - -c "wget -O ~/cinabox/cinabox.tar.gz http://github.com/thewoolleyman/cinabox/tarball/master" ci
 echo "  Unzipping cinabox..."
-sudo su - -c "tar --directory=~/cinabox --overwrite -zxvf ~/cinabox/cinabox.tar.gz" ci
-echo "  Running cinabox bootstrap_ruby.sh..."
-sudo su - -c "echo 'TODO: run boostrap_ruby.sh" ci
+sudo su - -c "tar --directory=/home/ci/cinabox --overwrite --strip-components=1 -zxvf ~/cinabox/cinabox.tar.gz" ci
 
+echo "  Downloading cinabox..."
 sudo su - -c "mkdir ~/railsci" ci
 sudo su - -c "wget -O ~/railsci/setup_rails_dependencies.rb {$RAILS_GIT_DOWNLOAD_URL}/ci/setup_rails_dependencies.rb" ci
