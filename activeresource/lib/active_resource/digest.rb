88a64799c0d6f156bca8754918c32ac4eed9c280
François Beausoleil <francois@teksol.info>
2009-08-04 15:01:54 -0400

    Connection uses Digest authentication when previous headers are available.

---
+++ activeresource/lib/active_resource/digest.rb
@@ -0,0 +1,50 @@
+module ActiveResource
+  # Written by Eric Hodel <drbrain@segment7.net>
+  # Copied and adapted from http://segment7.net/projects/ruby/snippets/digest_auth.rb
+  #
+  #  HTTP Digest Authentication
+  module Digest
+    @@nonce_count = -1
+
+    CNONCE = ::Digest::MD5.hexdigest("%x" % (Time.now.to_i + rand(65535)))
+    # CNONCE = ActiveSupport::SecureRandom.hex(32)
+
+    def self.authenticate(uri, user, password, auth_header, is_IIS = false)
+      uri = URI.parse(uri) unless uri.kind_of?(URI)
+      @@nonce_count += 1
+
+      auth_header =~ /^(\w+) (.*)/
+      raise ArgumentError, "Authentication method must be 'Digest', found #{$1}" unless $1 == "Digest"
+
+      params = {}
+      $2.gsub(/(\w+)="(.*?)"/) { params[$1] = $2 }
+
+      a_1 = "#{user}:#{params['realm']}:#{password}"
+      a_2 = "GET:#{uri.path}"
+      request_digest = ''
+      request_digest << ::Digest::MD5.hexdigest(a_1)
+      request_digest << ':' << params["nonce"]
+      request_digest << ':' << ("%08x" % @@nonce_count)
+      request_digest << ':' << CNONCE
+      request_digest << ':' << params["qop"]
+      request_digest << ':' << ::Digest::MD5.hexdigest(a_2)
+
+      header = ''
+      header << "Digest username=\"#{user}\", "
+      header << "realm=\"#{params["realm"]}\", "
+      if is_IIS then
+        header << "qop=\"#{params["qop"]}\", "
+      else
+        header << "qop=#{params["qop"]}, "
+      end
+      header << "uri=\"#{uri.path}\", "
+      header << "nonce=\"#{params["nonce"]}\", "
+      header << "nc=#{"%08x" % @@nonce_count}, "
+      header << "cnonce=\"#{CNONCE}\", "
+      header << "opaque=\"#{params["opaque"]}\", "
+      header << "response=\"#{::Digest::MD5.hexdigest(request_digest)}\""
+
+      return header
+    end
+  end
+end
