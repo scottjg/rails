f9a852e47be0e958cb28b5bebf14766607c63304
Bryan Helmkamp <bryan@brynary.com>
2009-08-30 13:34:09 -0400

    Use Bundler for ActiveModel development time dependencies

--- activemodel/lib/active_model.rb
+++ activemodel/lib/active_model.rb
@@ -21,8 +21,6 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #++
 
-activesupport_path = "#{File.dirname(__FILE__)}/../../activesupport/lib"
-$:.unshift(activesupport_path) if File.directory?(activesupport_path)
 require 'active_support'
 
 module ActiveModel
--- activemodel/test/cases/helper.rb
+++ activemodel/test/cases/helper.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'vendor', 'gems', 'environment'))
+
 $:.unshift(File.dirname(__FILE__) + '/../../lib')
 $:.unshift(File.dirname(__FILE__) + '/../../../activesupport/lib')
 
