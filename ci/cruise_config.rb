d24605ae2b95b81d3bcac6a07fd8f7532c0e0699
Chad Woolley <thewoolleyman@gmail.com>
2009-08-31 20:57:08 -0700

    disable ci emails on my branch

--- ci/cruise_config.rb
+++ ci/cruise_config.rb
@@ -1,7 +1,7 @@
 Project.configure do |project|
   # Send email notifications about broken and fixed builds to core mailing list
   if Socket.gethostname =~ /ci.rubyonrails.org/ && ENV['ENABLE_RAILS_CI_EMAILS'] == 'true'
-    project.email_notifier.emails = ['rubyonrails-core@googlegroups.com']
+    # project.email_notifier.emails = ['rubyonrails-core@googlegroups.com']
   end
 
   project.build_command = 'sudo gem update --system && ruby ci/ci_build.rb'
