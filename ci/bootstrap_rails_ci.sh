3fc422172a50d78664d072ed63452ec0a4b0369a
Chad Woolley <thewoolleyman@gmail.com>
2009-09-05 15:10:51 -0700

    pass -l instead of - to su, to make it use proper login on gentoo

--- ci/bootstrap_rails_ci.sh
+++ ci/bootstrap_rails_ci.sh
@@ -18,31 +18,31 @@ if [ -z $RAILS_GIT_BRANCHES ]; then RAILS_GIT_BRANCHES='master,2-3-stable'; fi
 if [ -z $CINABOX_GIT_DOWNLOAD_URL ]; then CINABOX_GIT_DOWNLOAD_URL='http://github.com/thewoolleyman/cinabox/tarball/master'; fi
 
 echo "  Downloading cinabox.tar.gz to /home/ci/cinabox..."
-sudo su - -c "mkdir ~/cinabox" ci
-sudo su - -c "wget -O ~/cinabox/cinabox.tar.gz $CINABOX_GIT_DOWNLOAD_URL" ci
+sudo su -l -c "mkdir ~/cinabox" ci
+sudo su -l -c "wget -O ~/cinabox/cinabox.tar.gz $CINABOX_GIT_DOWNLOAD_URL" ci
 echo "  Unzipping cinabox.tar.gz to /home/ci/cinabox..."
-sudo su - -c "tar --directory=/home/ci/cinabox --overwrite --strip-components=1 -zxvf ~/cinabox/cinabox.tar.gz" ci
+sudo su -l -c "tar --directory=/home/ci/cinabox --overwrite --strip-components=1 -zxvf ~/cinabox/cinabox.tar.gz" ci
 if [ ! $? = 0 ]; then echo "Unable to download and unzip cinabox" && exit; fi
 
 echo "  Running cinabox to bootstrap ruby..."
 DEFAULT_RUBY_VERSION=1.8.6-p287
 if [ -z $RUBY_VERSION ]; then RUBY_VERSION=$DEFAULT_RUBY_VERSION; fi
-sudo su - -c "RUBY_VERSION=$RUBY_VERSION FORCE_RUBY_UNINSTALL=true ~/cinabox/bootstrap_ruby.sh" ci
+sudo su -l -c "RUBY_VERSION=$RUBY_VERSION FORCE_RUBY_UNINSTALL=true ~/cinabox/bootstrap_ruby.sh" ci
 if [ ! $? = 0 ]; then echo "Unable to bootstrap ruby" && exit; fi
 
 echo "  Running cinabox to setup ci..."
-sudo su - -c "CRUISE_USER=ci NO_DAEMON_START=true ruby ~/cinabox/setup_ci.rb --force" ci
+sudo su -l -c "CRUISE_USER=ci NO_DAEMON_START=true ruby ~/cinabox/setup_ci.rb --force" ci
 if [ ! $? = 0 ]; then echo "Unable to setup ci" && exit; fi
 
 echo "  Downloading setup_rails_dependencies to /home/ci/railsci..."
-sudo su - -c "mkdir ~/railsci" ci
+sudo su -l -c "mkdir ~/railsci" ci
 
-sudo su - -c "wget -O ~/railsci/setup_rails_dependencies.rb $RAILS_GIT_DOWNLOAD_URL/ci/setup_rails_dependencies.rb" ci
-sudo su - -c "ruby ~/railsci/setup_rails_dependencies.rb" ci
+sudo su -l -c "wget -O ~/railsci/setup_rails_dependencies.rb $RAILS_GIT_DOWNLOAD_URL/ci/setup_rails_dependencies.rb" ci
+sudo su -l -c "ruby ~/railsci/setup_rails_dependencies.rb" ci
 if [ ! $? = 0 ]; then echo "Unable to setup rails dependencies" && exit; fi
 
-sudo su - -c "wget -O ~/railsci/setup_rails_ci_project.rb $RAILS_GIT_DOWNLOAD_URL/ci/setup_rails_ci_project.rb" ci
-sudo su - -c "RAILS_GIT_REPO_URL=$RAILS_GIT_REPO_URL RAILS_GIT_BRANCHES=$RAILS_GIT_BRANCHES ruby ~/railsci/setup_rails_ci_project.rb" ci
+sudo su -l -c "wget -O ~/railsci/setup_rails_ci_project.rb $RAILS_GIT_DOWNLOAD_URL/ci/setup_rails_ci_project.rb" ci
+sudo su -l -c "RAILS_GIT_REPO_URL=$RAILS_GIT_REPO_URL RAILS_GIT_BRANCHES=$RAILS_GIT_BRANCHES ruby ~/railsci/setup_rails_ci_project.rb" ci
 if [ ! $? = 0 ]; then echo "Unable to setup rails ci project" && exit; fi
 
 echo "  Rails Continuous Integration server setup complete!"
