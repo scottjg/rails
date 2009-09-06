d3bf545f8c2073f81b327c6571282bb13f5f8466
Chad Woolley <thewoolleyman@gmail.com>
2009-09-05 19:46:44 -0700

    gentoo support

--- ci/setup_rails_dependencies.rb
+++ ci/setup_rails_dependencies.rb
@@ -5,20 +5,39 @@ class SetupRailsDependencies
   def self.setup
     puts "Setting up Rails build dependencies..."
 
-    packages = %w{
-      sqlite
-      libsqlite-dev
-      sqlite3
-      libsqlite3-dev
-      mysql-server-5.0
-      libmysqlclient-dev
-      postgresql
-      postgresql-server-dev-8.3
-      libfcgi-dev
-      memcached
-    }
-    # Install packages using aptitude
-    run "sudo aptitude -y install #{packages.join(" ")}"
+    # Detect distro, only Gentoo and Debian currently supported
+    distro = system('which emerge') ? 'gentoo' : 'debian'
+
+    if distro = 'gentoo'
+      packages = %w{
+        sqlite
+        mysql
+        sudo emerge postgresql-server
+        fcgi
+        memcached
+      }
+    else
+      packages = %w{
+        sqlite
+        libsqlite-dev
+        sqlite3
+        libsqlite3-dev
+        mysql-server-5.0
+        libmysqlclient-dev
+        postgresql
+        postgresql-server-dev-8.3
+        libfcgi-dev
+        memcached
+      }
+    end
+    
+    # Install packages
+    
+    if distro = 'gentoo'
+      run "sudo emerge #{packages.join(" ")}"
+    else
+      run "sudo aptitude -y install #{packages.join(" ")}"
+    end
 
     # start services
     run "sudo /etc/init.d/mysql start"
