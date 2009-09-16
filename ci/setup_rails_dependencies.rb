32abca9591f701a902b3cacbebeb7aa8a7df2daa
Chad Woolley <thewoolleyman@gmail.com>
2009-09-16 00:41:21 -0700

    remove gentoo support from setup_rails_dependencies.rb

--- ci/setup_rails_dependencies.rb
+++ ci/setup_rails_dependencies.rb
@@ -5,35 +5,21 @@ class SetupRailsDependencies
   def self.setup
     puts "Setting up Rails build dependencies..."
 
-    # Detect distro, only Gentoo and Debian currently supported
-    distro = system('which emerge') ? 'gentoo' : 'debian'
-
-    if distro == 'gentoo'
-      packages = %w{
-        sqlite
-        mysql
-        postgresql-server
-        fcgi
-        memcached
-      }
-    else
-      packages = %w{
-        sqlite
-        libsqlite-dev
-        sqlite3
-        libsqlite3-dev
-        mysql-server-5.0
-        libmysqlclient-dev
-        postgresql
-        postgresql-server-dev-8.3
-        libfcgi-dev
-        memcached
-      }
-    end
+    packages = %w{
+      sqlite
+      libsqlite-dev
+      sqlite3
+      libsqlite3-dev
+      mysql-server-5.0
+      libmysqlclient-dev
+      postgresql
+      postgresql-server-dev-8.3
+      libfcgi-dev
+      memcached
+    }
     
     # custom pre-package installation
-    if distro == 'debian'
-      pw_file = <<-eos
+    pw_file = <<-eos
 Name: mysql-server/root_password
 Template: mysql-server/root_password
 Value: 
@@ -45,26 +31,15 @@ Template: mysql-server/root_password_again
 Value: 
 Owners: mysql-server-5.0
 Flags: seen
-      eos
+    eos
 
-      run "sudo su -c 'echo \"#{pw_file}\" > /var/cache/debconf/passwords.dat'"
-    end
+    run "sudo su -c 'echo \"#{pw_file}\" > /var/cache/debconf/passwords.dat'"
         
     # Install packages    
     packages.each do |package|
-      if distro == 'gentoo'
-        run "sudo emerge #{package}" unless system("qlist -I | grep #{package}")
-      else
-        run "sudo aptitude -y install #{package}" unless ((run "dpkg -l #{package}", false) =~ /ii  #{package}/)
-      end
+      run "sudo aptitude -y install #{package}" unless ((run "dpkg -l #{package}", false) =~ /ii  #{package}/)
     end
     
-    # custom post-package installation
-    if distro == 'gentoo'
-      run "echo 'Y' | sudo emerge postgresql-server --config"
-      run "sudo /etc/init.d/postgresql-8.3 start"
-    end
-
     # start services
     run "sudo /etc/init.d/mysql start"
 
