401941223be5603ccaac71edf75483f3a5d8c410
Chad Woolley <thewoolleyman@gmail.com>
2009-09-26 19:02:17 -0700

    add libxml/libxslt dependencies to support nokogiri

--- ci/setup_rails_dependencies.rb
+++ ci/setup_rails_dependencies.rb
@@ -16,10 +16,10 @@ class SetupRailsDependencies
       postgresql-server-dev-8.3
       libfcgi-dev
       memcached
-      libxml
-      libxml-dev
-      libxslt
-      libxslt-dev
+      libxml2
+      libxml2-dev
+      libxslt1
+      libxslt1-dev
     }
     
     # custom pre-package installation
