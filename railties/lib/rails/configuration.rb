364e65626da7e7bd46b3f69b79f5ba644a063c25
Bob Remeika <bob.remeika@gmail.com>
2009-09-30 17:25:55 -0700

    Added accessor

--- railties/lib/rails/configuration.rb
+++ railties/lib/rails/configuration.rb
@@ -10,7 +10,8 @@ module Rails
                   :log_path, :log_level, :logger, :preload_frameworks,
                   :database_configuration_file, :cache_store, :time_zone,
                   :view_path, :metals, :controller_paths, :routes_configuration_file,
-                  :eager_load_paths, :dependency_loading, :paths, :serve_static_assets
+                  :eager_load_paths, :dependency_loading, :paths, :serve_static_assets,
+                  :js_frameworks
 
     def initialize
       set_root_path!
