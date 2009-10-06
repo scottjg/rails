2f651dd46b3ef9b3288b242745504c05dc67243f
Michael Koziarski <michael@koziarski.com>
2009-10-06 16:32:27 +1300

    Also support << and .concat for safe strings, tidy up the implementation somewhat
    Conflicts:
    
    	activesupport/lib/active_support/core_ext/string/output_safety.rb

--- activesupport/lib/active_support/core_ext/string/output_safety.rb
+++ activesupport/lib/active_support/core_ext/string/output_safety.rb
@@ -1,7 +1,16 @@
-class String
-  def html_safe?
-    defined?(@_rails_html_safe) && @_rails_html_safe
-  end
+module ActiveSupport #:nodoc:
+  module CoreExtensions #:nodoc:
+    module String #:nodoc:
+      module OutputSafety
+        def self.included(base)
+          base.class_eval do
+            alias_method :add_without_safety, :+
+            alias_method :+, :add_with_safety
+            alias_method_chain :concat, :safety
+            undef_method :<<
+            alias_method :<<, :concat_with_safety
+          end
+        end
 
   def html_safe!
     @_rails_html_safe = true
