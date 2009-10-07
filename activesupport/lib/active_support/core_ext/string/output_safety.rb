d3a96e95b86aab9e89e70802fcd36c079108014f
Michael Koziarski <michael@koziarski.com>
2009-10-07 16:09:53 +1300

    Fix in the other methods which need to be safety aware

--- activesupport/lib/active_support/core_ext/string/output_safety.rb
+++ activesupport/lib/active_support/core_ext/string/output_safety.rb
@@ -11,4 +11,33 @@ class String
   def html_safe
     dup.html_safe!
   end
+  
+  alias original_plus +
+  def +(other)
+    result = original_plus(other)
+    if html_safe? && also_html_safe?(other)
+      result.html_safe!
+    else
+      result
+    end
+  end
+  
+  alias original_concat <<
+  def <<(other)
+    result = original_concat(other)
+    unless html_safe? && also_html_safe?(other)
+      @_rails_html_safe = false
+    end
+    result
+  end
+  
+  def concat(other)
+    self << other
+  end
+  
+  private
+    def also_html_safe?(other)
+      other.respond_to?(:html_safe?) && other.html_safe?
+    end
+  
 end
