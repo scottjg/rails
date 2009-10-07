e51777a2a2bc127a9d6f8be8d71f2a480df518e2
Michael Koziarski <michael@koziarski.com>
2009-10-07 14:54:31 +1300

    ERB::Util.h not CGI.escapeHTML

--- actionpack/test/controller/output_escaping_test.rb
+++ actionpack/test/controller/output_escaping_test.rb
@@ -2,18 +2,18 @@ require 'abstract_unit'
 
 class OutputEscapingTest < ActiveSupport::TestCase
 
-  test "escapeHTML shouldn't die when passed nil" do
-    assert_nil CGI.escapeHTML(nil)
+  test "escape_html shouldn't die when passed nil" do
+    assert ERB::Util.h(nil).blank?
   end
 
   test "escapeHTML should escape strings" do
-    assert_equal "&lt;&gt;&quot;", CGI.escapeHTML("<>\"")
+    assert_equal "&lt;&gt;&quot;", ERB::Util.h("<>\"")
   end
 
   test "escapeHTML shouldn't touch explicitly safe strings" do
     # TODO this seems easier to compose and reason about, but
     # this should be verified
-    assert_equal "<", CGI.escapeHTML("<".html_safe!)
+    assert_equal "<", ERB::Util.h("<".html_safe!)
   end
 
 end
