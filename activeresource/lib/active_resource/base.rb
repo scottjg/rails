75a4611643999c97e57e9bfd82f2f841e7a4b866
David Genord II <david@xspond.com>
2009-12-14 14:48:06 -0500

    Needs to handle nil body in case Content-Length is improperly reported

--- activeresource/lib/active_resource/base.rb
+++ activeresource/lib/active_resource/base.rb
@@ -1084,7 +1084,7 @@ module ActiveResource
       end
 
       def load_attributes_from_response(response)
-        if response['Content-Length'] != "0" && response.body.strip.size > 0
+        if response['Content-Length'] != "0" && response.body.to_s.strip.size > 0
           load(self.class.format.decode(response.body))
         end
       end
