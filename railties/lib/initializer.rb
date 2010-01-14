3b27953f0bd3eaaafc6b1fde46a3ada3729ec06c
David Genord II <david@xspond.com>
2010-01-14 11:15:04 -0500

    Initializers should be run before observers and routing. This is also the order from 2.3

--- railties/lib/initializer.rb
+++ railties/lib/initializer.rb
@@ -104,16 +104,16 @@ module Rails
 
       load_plugins
 
+      # the framework is now fully initialized
+      after_initialize
+
+      load_application_initializers
+
       # Observers are loaded after plugins in case Observers or observed models are modified by plugins.
       load_observers
 
       # Routing must be initialized after plugins to allow the former to extend the routes
       initialize_routing
-
-      # the framework is now fully initialized
-      after_initialize
-
-      load_application_initializers
     end
 
     # Check for valid Ruby version
