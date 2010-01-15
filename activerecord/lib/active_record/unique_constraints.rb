14feddfdda9a7fd3709c099a458bd4f8e81aea59
Jordan Brough <jordan@animoto.com>
2010-01-15 15:10:55 -0700

    Add bang method handling to db-enforced unique constraints

--- activerecord/lib/active_record/unique_constraints.rb
+++ activerecord/lib/active_record/unique_constraints.rb
@@ -4,6 +4,7 @@ module ActiveRecord
     def self.included(base)
       base.class_eval do
         alias_method_chain :save, :unique_constraints
+        alias_method_chain :save!, :unique_constraints
       end
     end
 
@@ -16,15 +17,28 @@ module ActiveRecord
       cache_indexes
       save_without_unique_constraints(perform_validation)
     rescue ActiveRecord::RecordNotUnique => e
-      index = connection.index_for_record_not_unique(e.original_exception, self.class.table_name)
-      if !index
-        errors.add_to_base(I18n.translate(:'activerecord.errors.messages.taken_generic'))
-      elsif index.columns.size == 1
-        errors.add(index.columns.first, :taken, :value => attributes[index.columns.first])
-      else
-        errors.add(index.columns.first, :taken_multiple, :context => index.columns.slice(1..-1).join('/'), :value => attributes[index.columns.first])
-      end
+      parse_unique_exception(e)
       false
     end
+
+    def save_with_unique_constraints! #:nodoc:
+      cache_indexes
+      save_without_unique_constraints!
+    rescue ActiveRecord::RecordNotUnique => e
+      parse_unique_exception(e)
+      raise RecordInvalid.new(self)
+    end
+
+    protected
+      def parse_unique_exception(e)
+        index = connection.index_for_record_not_unique(e.original_exception, self.class.table_name)
+        if !index
+          errors.add_to_base(I18n.translate(:'activerecord.errors.messages.taken_generic'))
+        elsif index.columns.size == 1
+          errors.add(index.columns.first, :taken, :value => attributes[index.columns.first])
+        else
+          errors.add(index.columns.first, :taken_multiple, :context => index.columns.slice(1..-1).join('/'), :value => attributes[index.columns.first])
+        end
+      end
   end
 end
