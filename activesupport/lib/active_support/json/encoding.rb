a2c66e1137e0046a268a7e3713da0d487d4ac875
Ubiratan Pires Alberton <u.alberton@gmail.com>
2010-01-22 11:59:57 -0200

    [#1112] Fix "redundant UTF08 sequence" in String#to_json
    This is an update of a patch submitted by Jamis Buck to Rails Lightouse ticket #1112

--- activesupport/lib/active_support/json/encoding.rb
+++ activesupport/lib/active_support/json/encoding.rb
@@ -53,7 +53,7 @@ module ActiveSupport
           string = string.dup.force_encoding(::Encoding::BINARY) if string.respond_to?(:force_encoding)
           json = string.
             gsub(escape_regex) { |s| ESCAPED_CHARS[s] }.
-            gsub(/([\xC0-\xDF][\x80-\xBF]|
+            gsub(/([\xC2-\xDF][\x80-\xBF]|
                    [\xE0-\xEF][\x80-\xBF]{2}|
                    [\xF0-\xF7][\x80-\xBF]{3})+/nx) { |s|
             s.unpack("U*").pack("n*").unpack("H*")[0].gsub(/.{4}/n, '\\\\u\&')
