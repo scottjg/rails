cb766bf6f31c529a048d8e17d83e59323a2ce695
Kevin Valdek <kevinvaldek@gmail.com>
2010-01-28 01:35:25 +0200

    moved Request.Rails out of the domready callback.

--- railties/lib/generators/rails/app/templates/public/javascripts/mootools.driver.js
+++ railties/lib/generators/rails/app/templates/public/javascripts/mootools.driver.js
@@ -1,86 +1,5 @@
 window.addEvent('domready', function($) {
 
-  Request.Rails = new Class({
-
-    Extends: Request,
-
-    options: {
-      update: null,
-      position: null
-    },
-
-    initialize: function(element) {
-      this.el = element;
-      if(!this.conditionMet()) return;
-
-      this.parent({
-        method: this.el.get('method') || this.el.get('data-method') || 'get',
-        url: this.el.get('action') || this.el.get('data-url') || '#',
-        async: this.el.get('data-remote-type') !== 'synchronous',
-        update: $(this.el.get('data-update-success')),
-        position: this.el.get('data-update-position')
-      });
-      this.headers['Accept'] = '*/*';
-
-      this.setData();
-      this.addRailsEvents();
-    },
-
-    send: function(options) {
-      this.el.fireEvent('rails:before');
-      this.parent(options);
-    },
-
-    addRailsEvents: function() {
-      this.addEvent('request', function() {
-        this.el.fireEvent('rails:after', this.xhr);
-        this.el.fireEvent('rails:loading', this.xhr);
-      });
-
-      this.addEvent('success', function(responseText) {
-        this.el.fireEvent('rails:success', this.xhr);
-
-        if(this.options.update) {
-          if(this.options.position) {
-            new Element('div', {
-              html: responseText
-            }).inject(this.options.update, this.options.position);
-          } else {
-            this.options.update.set('html', responseText);
-          }
-        }
-      });
-
-      this.addEvent('complete', function() {
-        this.el.fireEvent('rails:complete', this.xhr);
-        this.el.fireEvent('rails:loaded', this.xhr);
-      });
-
-      this.addEvent('failure', function() {
-        this.el.fireEvent('rails:failure', this.xhr);
-      });
-    },
-
-    setData: function() {
-      if (this.el.get('data-submit')) {
-        this.options.data = $(this.el.get('data-submit'));
-      } else if (this.el.get('data-with')) {
-        this.options.data = this.el.get('data-with');
-      } else if(this.el.get('tag') == 'form') {
-        this.options.data = this.el;
-      } else if(this.el.get('tag') == 'input') {
-        this.options.data = this.el.getParent('form');
-      }
-    },
-
-    conditionMet: function() {
-      var condition = this.el.get('data-condition');
-      if(condition) return eval(condition);
-      return true;
-    }
-    
-  });
-
   var handleRemote = function(e) {
     e.preventDefault();
     new Request.Rails(this).send();
@@ -96,6 +15,88 @@ window.addEvent('domready', function($) {
     $$(split[0]).addEvent(split[1], hooks[key]);
   }
 }.pass(document.id));
+
+
+Request.Rails = new Class({
+
+  Extends: Request,
+
+  options: {
+    update: null,
+    position: null
+  },
+
+  initialize: function(element) {
+    this.el = element;
+    if(!this.conditionMet()) return;
+
+    this.parent({
+      method: this.el.get('method') || this.el.get('data-method') || 'get',
+      url: this.el.get('action') || this.el.get('data-url') || '#',
+      async: this.el.get('data-remote-type') !== 'synchronous',
+      update: $(this.el.get('data-update-success')),
+      position: this.el.get('data-update-position')
+    });
+    this.headers['Accept'] = '*/*';
+
+    this.setData();
+    this.addRailsEvents();
+  },
+
+  send: function(options) {
+    this.el.fireEvent('rails:before');
+    this.parent(options);
+  },
+
+  addRailsEvents: function() {
+    this.addEvent('request', function() {
+      this.el.fireEvent('rails:after', this.xhr);
+      this.el.fireEvent('rails:loading', this.xhr);
+    });
+
+    this.addEvent('success', function(responseText) {
+      this.el.fireEvent('rails:success', this.xhr);
+
+      if(this.options.update) {
+        if(this.options.position) {
+          new Element('div', {
+            html: responseText
+          }).inject(this.options.update, this.options.position);
+        } else {
+          this.options.update.set('html', responseText);
+        }
+      }
+    });
+
+    this.addEvent('complete', function() {
+      this.el.fireEvent('rails:complete', this.xhr);
+      this.el.fireEvent('rails:loaded', this.xhr);
+    });
+
+    this.addEvent('failure', function() {
+      this.el.fireEvent('rails:failure', this.xhr);
+    });
+  },
+
+  setData: function() {
+    if (this.el.get('data-submit')) {
+      this.options.data = $(this.el.get('data-submit'));
+    } else if (this.el.get('data-with')) {
+      this.options.data = this.el.get('data-with');
+    } else if(this.el.get('tag') == 'form') {
+      this.options.data = this.el;
+    } else if(this.el.get('tag') == 'input') {
+      this.options.data = this.el.getParent('form');
+    }
+  },
+
+  conditionMet: function() {
+    var condition = this.el.get('data-condition');
+    if(condition) return eval(condition);
+    return true;
+  }
+
+});
 
 /**
  * MooTools selector engine does not match data-* attributes.
