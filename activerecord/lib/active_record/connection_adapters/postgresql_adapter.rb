5bfecc1652c219051b3b316531177b7fafca774d
Miles Georgi <azimux@gmail.com>
2010-03-03 21:47:34 -0800

    Override index_name in postgresql_adapter to ensure that index names are truncated regardless of PostgreSQL driver.

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -416,6 +416,13 @@ module ActiveRecord
         quote_string(s)
       end
 
+      #There are PostgreSQL drivers/versions of drivers that truncate
+      #index names to 63 characters.  Since some do not, this override makes
+      #sure that the truncate occurs regardless of PostgreSQL driver.
+      def index_name(table, columns)
+        super(table, columns)[0..62]
+      end
+
       # Checks the following cases:
       #
       # - table_name
