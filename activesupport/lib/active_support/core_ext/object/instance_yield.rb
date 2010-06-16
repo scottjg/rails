f1c868d45cc763354b326be3b4e240667b636123
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-06-16 12:57:56 +0200

    switched from instance_eval to instance_exec for instance_yield, so it can take additional arguments

--- activesupport/lib/active_support/core_ext/object/instance_yield.rb
+++ activesupport/lib/active_support/core_ext/object/instance_yield.rb
@@ -1,22 +1,28 @@
 class Object
-  # Leaves the decission to use instance_eval for internal DSLs up the end user
-  # by using instance_eval if the given block takes an argument, yield(self)
-  # otherwise.
+  # Leaves the decission to use instance_exec for internal DSLs up the end user
+  # by using instance_exec if the given block takes more arguments than passed to
+  # instance_yield, yield(self, *args) otherwise.
   #
   #   def my_dsl(&block)
-  #     dsl_object.instance_yield(&block)
+  #     dsl_object.instance_yield(42, &block)
   #   end
   #   
-  #   my_dsl do
+  #   my_dsl do |number|
   #     do_something :with => "a string"
-  #     self # => dsl_object
+  #     self    # => dsl_object
+  #     number  # => 42
   #   end
   #   
-  #   my_dsl do |c|
-  #     c.do_something :with => "a string"
-  #     self # => main
+  #   my_dsl do |object, number|
+  #     object.do_something :with => "a string"
+  #     self    # => main
+  #     number  # => 42
   #   end
-  def instance_yield(&block)
-    block.arity > 0 ? yield(self) : instance_eval(&block)
+  def instance_yield(*args, &block)
+    if block.arity > args.size or (-1 - block.arity) > args.size
+      yield(self, *args)
+    else
+      instance_exec(*args, &block)
+    end
   end
 end
--- activesupport/test/core_ext/object/instance_yield_test.rb
+++ activesupport/test/core_ext/object/instance_yield_test.rb
@@ -1,11 +1,23 @@
 require 'active_support/core_ext/object/instance_yield'
 
 class InstanceYieldTest < Test::Unit::TestCase
-  def test_instance_eval
+  def test_instance_exec
     assert_equal "foo", "foo".instance_yield { self }
   end
 
   def test_yield
     assert_equal self, "foo".instance_yield { |s| self }
+  end
+
+  def test_intance_exec_with_argument
+    assert_equal 42, instance_yield(42) { |a| 42 }
+    assert_equal "foo", "foo".instance_yield(10) { |a| self }
+    assert_equal "foo", "foo".instance_yield(10, 20) { |a, b| self }
+  end
+
+  def test_yield_with_argument
+    assert_equal 42, instance_yield(42) { |s, a| 42 }
+    assert_equal self, "foo".instance_yield(10) { |s, a| self }
+    assert_equal self, "foo".instance_yield(10, 20) { |s, a, b| self }
   end
 end
