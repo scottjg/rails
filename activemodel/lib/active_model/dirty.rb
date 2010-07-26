9a27125dc25c3c3c551b9130d6a8e8f0c24ac144
Tore Darell <toredarell@gmail.com>
2010-07-26 16:46:48 +0200

    Remove or fix non-working examples and add a few tests to Dirty

--- activemodel/lib/active_model/dirty.rb
+++ activemodel/lib/active_model/dirty.rb
@@ -37,12 +37,13 @@ module ActiveModel
   #     end
   #   
   #     def name=(val)
-  #       name_will_change!
+  #       name_will_change! unless val == @name
   #       @name = val
   #     end
   #   
   #     def save
   #       @previously_changed = changes
+  #       @changed_attributes.clear
   #     end
   #   
   #   end
@@ -76,12 +77,6 @@ module ActiveModel
   #   person.name = 'Bob'
   #   person.changed        # => ['name']
   #   person.changes        # => { 'name' => ['Bill', 'Bob'] }
-  #
-  # Resetting an attribute returns it to its original state:
-  #   person.reset_name!    # => 'Bill'
-  #   person.changed?       # => false
-  #   person.name_changed?  # => false
-  #   person.name           # => 'Bill'
   #
   # Before modifying an attribute in-place:
   #   person.name_will_change!
