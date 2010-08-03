866413213f80c044241a47a8395d252b633dbaec
Piotr Sarnacki <drogus@gmail.com>
2010-08-03 19:36:53 +0200

    Refactor ActionMailer to not use hide_actions

--- actionmailer/lib/action_mailer/base.rb
+++ actionmailer/lib/action_mailer/base.rb
@@ -5,7 +5,6 @@ require 'active_support/core_ext/array/wrap'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/proc'
 require 'action_mailer/log_subscriber'
-require 'action_mailer/hide_actions'
 
 module ActionMailer #:nodoc:
   # Action Mailer allows you to send email from your application using a mailer model and views.
@@ -341,12 +340,12 @@ module ActionMailer #:nodoc:
     include AbstractController::Helpers
     include AbstractController::Translation
     include AbstractController::AssetPaths
+    include AbstractController::UrlFor
 
     helper  ActionMailer::MailHelper
 
     include ActionMailer::OldApi
     include ActionMailer::DeprecatedApi
-    include ActionMailer::HideActions
 
     delegate :register_observer, :to => Mail
     delegate :register_interceptor, :to => Mail
@@ -363,9 +362,8 @@ module ActionMailer #:nodoc:
 
     class << self
       def inherited(klass)
-        klass.with_hiding_actions do
-          super(klass)
-        end
+        super(klass)
+        klass.class_eval { @action_methods = nil }
       end
 
       def mailer_name
@@ -726,9 +724,6 @@ module ActionMailer #:nodoc:
       part = Mail::Part.new(response)
       container.add_part(part)
     end
-
-    class_attribute :default_url_options
-    self.default_url_options = {}
 
     ActiveSupport.run_load_hooks(:action_mailer, self)
   end
