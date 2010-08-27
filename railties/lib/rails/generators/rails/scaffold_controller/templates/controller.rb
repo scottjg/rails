89e685474cee8b9e4877b458717ac509a853baa1
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2010-08-27 16:44:28 +0200

    Changes scaffold's template to use responders API

--- railties/lib/rails/generators/rails/scaffold_controller/templates/controller.rb
+++ railties/lib/rails/generators/rails/scaffold_controller/templates/controller.rb
@@ -1,13 +1,11 @@
 class <%= controller_class_name %>Controller < ApplicationController
+  respond_to :html, :xml
   # GET <%= route_url %>
   # GET <%= route_url %>.xml
   def index
     @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @<%= plural_table_name %> }
-    end
+    respond_with(@<%= plural_table_name %>)
   end
 
   # GET <%= route_url %>/1
@@ -15,10 +13,7 @@ class <%= controller_class_name %>Controller < ApplicationController
   def show
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @<%= singular_table_name %> }
-    end
+    respond_with(@<%= singular_table_name %>)
   end
 
   # GET <%= route_url %>/new
@@ -26,10 +21,7 @@ class <%= controller_class_name %>Controller < ApplicationController
   def new
     @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @<%= singular_table_name %> }
-    end
+    respond_with(@<%= singular_table_name %>)
   end
 
   # GET <%= route_url %>/1/edit
@@ -42,15 +34,8 @@ class <%= controller_class_name %>Controller < ApplicationController
   def create
     @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
 
-    respond_to do |format|
-      if @<%= orm_instance.save %>
-        format.html { redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully created.') }
-        format.xml  { render :xml => @<%= singular_table_name %>, :status => :created, :location => @<%= singular_table_name %> }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @<%= orm_instance.errors %>, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "<%= human_name %> was successfully created." if @<%= orm_instance.save %>
+    respond_with(@<%= singular_table_name %>)
   end
 
   # PUT <%= route_url %>/1
@@ -58,15 +43,8 @@ class <%= controller_class_name %>Controller < ApplicationController
   def update
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
 
-    respond_to do |format|
-      if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
-        format.html { redirect_to(@<%= singular_table_name %>, :notice => '<%= human_name %> was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @<%= orm_instance.errors %>, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "<%= human_name %> was successfully updated." if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
+    respond_with(@<%= singular_table_name %>)
   end
 
   # DELETE <%= route_url %>/1
@@ -75,9 +53,6 @@ class <%= controller_class_name %>Controller < ApplicationController
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
     @<%= orm_instance.destroy %>
 
-    respond_to do |format|
-      format.html { redirect_to(<%= index_helper %>_url) }
-      format.xml  { head :ok }
-    end
+    respond_with(@<%= singular_table_name %>)
   end
 end
