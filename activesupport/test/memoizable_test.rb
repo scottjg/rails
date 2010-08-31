4d1a36fb952bb63858657f7269bad6d14f85791b
Jason Frey <fryguy9@gmail.com>
2010-08-31 18:01:03 -0400

    Improved performance of flush_cache and prime_cache by storing the memoized methods.

--- activesupport/test/memoizable_test.rb
+++ activesupport/test/memoizable_test.rb
@@ -109,6 +109,10 @@ class MemoizableTest < ActiveSupport::TestCase
     assert_equal 1, @person.name_calls
   end
 
+  def test_memoized_methods
+    assert_equal %w{age is_developer? name name? update}, Person.memoized_methods.sort
+  end
+
   def test_memoization_with_punctuation
     assert_equal true, @person.name?
 
@@ -197,6 +201,7 @@ class MemoizableTest < ActiveSupport::TestCase
       company.extend ActiveSupport::Memoizable
       company.memoize :name
 
+      assert_equal %w{name}, company.memoized_methods
       assert_equal "37signals", company.name
       assert_equal 1, company.name_calls
       assert_equal "37signals", company.name
