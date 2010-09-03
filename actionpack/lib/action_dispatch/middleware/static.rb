f9bfaf1c2f81a75499a876a07ce994194dc78c53
Piotr Sarnacki <drogus@gmail.com>
2010-09-03 12:48:21 +0200

    Optimize ActionDispatch::Static

--- actionpack/lib/action_dispatch/middleware/static.rb
+++ actionpack/lib/action_dispatch/middleware/static.rb
@@ -2,43 +2,12 @@ require 'rack/utils'
 
 module ActionDispatch
   class Static
-    class FileHandler
-      def initialize(at, root)
-        @at = at.chomp("/")
-        @file_server = ::Rack::File.new(root)
-      end
-
-      def file_exist?(path)
-        (path = full_readable_path(path)) && File.file?(path)
-      end
-
-      def directory_exist?(path)
-        (path = full_readable_path(path)) && File.directory?(path)
-      end
-
-      def call(env)
-        env["PATH_INFO"].gsub!(/^#{@at}/, "")
-        @file_server.call(env)
-      end
-
-      private
-        def includes_path?(path)
-          @at == "" || path =~ /^#{@at}/
-        end
-
-        def full_readable_path(path)
-          return unless includes_path?(path)
-          path = path.gsub(/^#{@at}/, "")
-          File.join(@file_server.root, ::Rack::Utils.unescape(path))
-        end
-    end
-
     FILE_METHODS = %w(GET HEAD).freeze
 
     def initialize(app, roots)
       @app = app
-      roots = normalize_roots(roots)
-      @file_handlers = file_handlers(roots)
+      @roots = normalize_roots(roots)
+      @file_servers = create_file_servers
     end
 
     def call(env)
@@ -46,15 +15,21 @@ module ActionDispatch
       method = env['REQUEST_METHOD']
 
       if FILE_METHODS.include?(method)
-        if file_handler = file_exist?(path)
-          return file_handler.call(env)
-        else
-          cached_path = directory_exist?(path) ? "#{path}/index" : path
-          cached_path += ::ActionController::Base.page_cache_extension
+        ext = ::ActionController::Base.page_cache_extension
 
-          if file_handler = file_exist?(cached_path)
-            env['PATH_INFO'] = cached_path
-            return file_handler.call(env)
+        @roots.each do |at, root|
+          if at == "" || path =~ /^#{at}/
+            full_path = File.join(root, ::Rack::Utils.unescape(path.sub(/^#{at}/, "")))
+            paths = "{#{[full_path, "#{full_path}#{ext}", "#{full_path}/index#{ext}"].join(",")}}"
+
+            matches = Dir[paths]
+            unless matches.blank?
+              match = matches.detect { |m| File.file?(m) }
+              if match
+                env["PATH_INFO"] = match.sub(/^#{root}/, '')
+                return @file_servers[at].call(env)
+              end
+            end
           end
         end
       end
@@ -63,22 +38,24 @@ module ActionDispatch
     end
 
     private
-      def file_exist?(path)
-        @file_handlers.detect { |f| f.file_exist?(path) }
-      end
-
-      def directory_exist?(path)
-        @file_handlers.detect { |f| f.directory_exist?(path) }
-      end
-
       def normalize_roots(roots)
-        roots.is_a?(Hash) ? roots : { "/" => roots.chomp("/") }
+        roots = { "" => roots.chomp("/") } unless roots.is_a?(Hash)
+
+        new_roots = ActiveSupport::OrderedHash.new
+        roots.each do |at, root|
+          new_roots[at.chomp('/')] = root if File.directory?(root)
+        end
+
+        new_roots
       end
 
-      def file_handlers(roots)
-        roots.map do |at, root|
-          FileHandler.new(at, root)
+      def create_file_servers
+        servers = {}
+        @roots.each do |at, root|
+          servers[at] = ::Rack::File.new(root)
         end
+
+        servers
       end
   end
 end
