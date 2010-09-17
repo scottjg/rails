f5f161195c17195f00ca1a2a381e4084c4d0517d
Emilio Tagua <miloops@gmail.com>
2010-09-17 16:00:06 -0300

    Don't use identity map if loading readonly records, this will prevent changing readonly status on already loaded records.

--- activerecord/lib/active_record/relation.rb
+++ activerecord/lib/active_record/relation.rb
@@ -61,7 +61,15 @@ module ActiveRecord
     def to_a
       return @records if loaded?
 
-      @records = eager_loading? ? find_with_associations : @klass.find_by_sql(arel.to_sql)
+      readonly = @readonly_value.nil? ? @implicit_readonly : @readonly_value
+
+      @records = if readonly
+        IdentityMap.without do
+          eager_loading? ? find_with_associations : @klass.find_by_sql(arel.to_sql)
+        end
+      else
+        eager_loading? ? find_with_associations : @klass.find_by_sql(arel.to_sql)
+      end
 
       preload = @preload_values
       preload +=  @includes_values unless eager_loading?
@@ -69,7 +77,6 @@ module ActiveRecord
 
       # @readonly_value is true only if set explicitly. @implicit_readonly is true if there
       # are JOINS and no explicit SELECT.
-      readonly = @readonly_value.nil? ? @implicit_readonly : @readonly_value
       @records.each { |record| record.readonly! } if readonly
 
       @loaded = true
