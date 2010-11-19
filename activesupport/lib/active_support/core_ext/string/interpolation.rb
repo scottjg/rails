85a05662fe7e0b06e9c7cd93cae9c3721058d879
Sven Fuchs <svenfuchs@artweb-design.de>
2010-11-19 09:23:15 +0100

    Move 1.9 string interpolation syntax backport from I18n to ActiveSupport
    In I18n we've shipped a String#% patch for a number of versions which backports 1.9's string interpolation syntax to 1.8. At some point Rails started relying on this behavior so it seems appropriate to move it to ActiveSupport (where people most likely will look for such extensions). 
    
    In I18n itself we're going to not rely on this patch anymore but implement our own version of I18n.interpolate, mostly in order to get rid of warnings (and in debug mode exceptions) that are raised when optional interpolation variables are passed. (Optional interpolation variables are something we absolutely need in I18n but seems to be seen as a wrong usage of sprintf by Ruby core.)

--- activesupport/lib/active_support/core_ext/string/interpolation.rb
+++ activesupport/lib/active_support/core_ext/string/interpolation.rb
@@ -1,2 +1,46 @@
-require 'active_support/i18n'
-require 'i18n/core_ext/string/interpolate'
+# heavily based on Masao Mutoh's gettext String interpolation extension
+# http://github.com/mutoh/gettext/blob/f6566738b981fe0952548c421042ad1e0cdfb31e/lib/gettext/core_ext/string.rb
+
+# KeyError is raised by String#% when the string contains a named placeholder that is
+# not contained in the given arguments hash. Ruby 1.9 defines and raises this exception
+# natively. We define it to mimic Ruby 1.9's behaviour in Ruby 1.8.x
+class KeyError < IndexError
+  def initialize(message = nil)
+    super(message || "key not found")
+  end
+end unless defined?(KeyError)
+
+class String
+  INTERPOLATION_PATTERN = Regexp.union(
+    /%\{(\w+)\}/,                               # matches placeholders like "%{foo}"
+    /%<(\w+)>(.*?\d*\.?\d*[bBdiouxXeEfgGcps])/  # matches placeholders like "%<foo>.d"
+  )
+
+  INTERPOLATION_PATTERN_WITH_ESCAPE = Regexp.union(
+    /%%/,
+    INTERPOLATION_PATTERN
+  )
+
+  alias :interpolate_without_ruby_19_syntax :%  # :nodoc:
+
+  # Backports the Ruby 1.9 string interpolation syntax to Ruby 1.8.
+  # See the Ruby 1.9 sprintf documentation for details.
+  def %(args)
+    if args.kind_of?(Hash)
+      dup.gsub(INTERPOLATION_PATTERN_WITH_ESCAPE) do |match|
+        if match == '%%'
+          '%'
+        else
+          key = ($1 || $2).to_sym
+          raise KeyError unless args.has_key?(key)
+          $3 ? sprintf("%#{$3}", args[key]) : args[key]
+        end
+      end
+    elsif self =~ INTERPOLATION_PATTERN
+      raise ArgumentError.new('one hash required')
+    else
+      result = gsub(/%([{<])/, '%%\1')
+      result.send :'interpolate_without_ruby_19_syntax', args
+    end
+  end
+end
