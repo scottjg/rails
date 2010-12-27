09a18b1bf00369b941ccb837e8bcbd2f17b909bb
bartes <bartosz.knapik@llp.pl>
2010-12-27 15:21:36 +0100

    attempt to remove strang chars from urls

--- activesupport/lib/active_support/core_ext/object/conversions.rb
+++ activesupport/lib/active_support/core_ext/object/conversions.rb
@@ -10,6 +10,11 @@ class Object
   # Note: This method is defined as a default implementation for all Objects for Hash#to_query to work.
   def to_query(key)
     require 'cgi' unless defined?(CGI) && defined?(CGI::escape)
-    "#{CGI.escape(key.to_s)}=#{CGI.escape(to_param.to_s)}"
+    begin
+      "#{CGI.escape(key.to_s)}=#{CGI.escape(to_param.to_s)}"
+    rescue
+      require 'active_support/inflector' unless defined?(ActiveSupport::Inflector)
+      "#{CGI.escape(key.to_s)}=#{CGI.escape(ActiveSupport::Inflector.transliterate(to_param.to_s))}"
+    end
   end
 end
