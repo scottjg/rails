1f6c064a71ccd3ce5a7d9b2156ac7fd7c0aaa8ec
Gavin Brock <gavin@kvh-ms.com>
2011-03-01 09:15:46 +0900

    Added patch for tsvector type (refs #6073) https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/6073

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -104,6 +104,9 @@ module ActiveRecord
             # UUID type
             when 'uuid'
               :string
+            # TSVector type
+            when 'tsvector'
+              :tsvector
             # Small and big integer types
             when /^(?:small|big)int$/
               :integer
@@ -203,7 +206,8 @@ module ActiveRecord
         :date        => { :name => "date" },
         :binary      => { :name => "bytea" },
         :boolean     => { :name => "boolean" },
-        :xml         => { :name => "xml" }
+        :xml         => { :name => "xml" },
+        :tsvector    => { :name => "tsvector" }
       }
 
       # Returns 'PostgreSQL' as adapter name for identification purposes.
