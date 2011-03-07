beec2f856b467c441c19f7cc86bf14d52c580465
J Smith <dark.panda@gmail.com>
2011-03-07 17:57:09 -0500

    Make session and environment dumping configurable options.

--- actionpack/lib/action_dispatch/railtie.rb
+++ actionpack/lib/action_dispatch/railtie.rb
@@ -8,5 +8,7 @@ module ActionDispatch
     config.action_dispatch.ip_spoofing_check = true
     config.action_dispatch.show_exceptions = true
     config.action_dispatch.best_standards_support = true
+    config.action_dispatch.dump_session_on_error = true
+    config.action_dispatch.dump_environment_on_error = false
   end
 end
