effb1352666082c7ff7e1ab2b584b6dfd95df892
Eric Allam <rubymaverick@gmail.com>
2011-05-17 12:16:18 -0400

    Use Marshal to do a deep clone of values in the cache store.  Fixes issue https://github.com/rails/rails/issues/545

--- actionpack/lib/action_dispatch/http/rack_cache.rb
+++ actionpack/lib/action_dispatch/http/rack_cache.rb
@@ -12,12 +12,25 @@ module ActionDispatch
     def initialize(store = RAILS_CACHE)
       @store = store
     end
-
+    
+    # since #write does a Marshal.dump on the value
+    # before writing it to the store, we need to
+    # Marshal.load it after returning it from the store
     def read(key)
-      @store.read(key) || []
+      if data = @store.read(key)
+        Marshal.load(data)
+      else
+        []
+      end
     end
 
+    # Marshal.dump the value to do a 'deep_clone'
+    # of the value so using MemoryStore doesn't cause
+    # modifications to the value after it is read from the store
+    # to persist across requests.  Fixes this issue:
+    # https://github.com/rails/rails/issues/545
     def write(key, value)
+      value = Marshal.dump(value)
       @store.write(key, value)
     end
 
