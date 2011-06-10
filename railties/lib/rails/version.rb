3b868723b02ad003303152e64234023db4b27900
Christopher Sun <christopher.sun@yahoo.com>
2011-06-10 13:48:08 -0400

    [bugid:2738] define? check

--- railties/lib/rails/version.rb
+++ railties/lib/rails/version.rb
@@ -1,9 +1,9 @@
 module Rails
   module VERSION #:nodoc:
-    MAJOR = 2
-    MINOR = 3
-    TINY  = 9
+    MAJOR = 2 unless defined?(MAJOR)
+    MINOR = 3 unless defined?(MINOR)
+    TINY  = 9 unless defined?(TINY)
 
-    STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING = [MAJOR, MINOR, TINY].join('.') unless defined?(STRING)
   end
 end
