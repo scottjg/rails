d8cf1e17314a91467669c314de75913c4e718aaa
Jeffrey Hardy <packagethief@gmail.com>
2011-06-10 11:53:02 -0400

    Flip the order of equality assertions to match the assert_equal(expected, actual) convention

--- activesupport/test/core_ext/time_ext_test.rb
+++ activesupport/test/core_ext/time_ext_test.rb
@@ -590,21 +590,22 @@ class TimeExtCalculationsTest < ActiveSupport::TestCase
   end
 
   def test_time_with_datetime_fallback
-    assert_equal Time.time_with_datetime_fallback(:utc, 2005, 2, 21, 17, 44, 30), Time.utc(2005, 2, 21, 17, 44, 30)
-    assert_equal Time.time_with_datetime_fallback(:local, 2005, 2, 21, 17, 44, 30), Time.local(2005, 2, 21, 17, 44, 30)
-    assert_equal Time.time_with_datetime_fallback(:utc, 2039, 2, 21, 17, 44, 30), DateTime.civil(2039, 2, 21, 17, 44, 30, 0, 0)
-    assert_equal Time.time_with_datetime_fallback(:local, 2039, 2, 21, 17, 44, 30), DateTime.civil(2039, 2, 21, 17, 44, 30, DateTime.local_offset, 0)
-    assert_equal Time.time_with_datetime_fallback(:utc, 1900, 2, 21, 17, 44, 30), DateTime.civil(1900, 2, 21, 17, 44, 30, 0, 0)
-    assert_equal Time.time_with_datetime_fallback(:utc, 2005), Time.utc(2005)
-    assert_equal Time.time_with_datetime_fallback(:utc, 2039), DateTime.civil(2039, 1, 1, 0, 0, 0, 0, 0)
-    assert_equal Time.time_with_datetime_fallback(:utc, 2005, 2, 21, 17, 44, 30, 1), Time.utc(2005, 2, 21, 17, 44, 30, 1) #with usec
+    assert_equal Time.utc(2005, 2, 21, 17, 44, 30), Time.time_with_datetime_fallback(:utc, 2005, 2, 21, 17, 44, 30)
+    assert_equal Time.local(2005, 2, 21, 17, 44, 30), Time.time_with_datetime_fallback(:local, 2005, 2, 21, 17, 44, 30)
+    assert_equal DateTime.civil(2039, 2, 21, 17, 44, 30, 0, 0), Time.time_with_datetime_fallback(:utc, 2039, 2, 21, 17, 44, 30)
+    assert_equal DateTime.civil(2039, 2, 21, 17, 44, 30, DateTime.local_offset, 0), Time.time_with_datetime_fallback(:local, 2039, 2, 21, 17, 44, 30)
+    assert_equal DateTime.civil(1900, 2, 21, 17, 44, 30, 0, 0), Time.time_with_datetime_fallback(:utc, 1900, 2, 21, 17, 44, 30)
+    assert_equal Time.utc(2005), Time.time_with_datetime_fallback(:utc, 2005)
+    assert_equal DateTime.civil(2039, 1, 1, 0, 0, 0, 0, 0), Time.time_with_datetime_fallback(:utc, 2039)
+    assert_equal Time.utc(2005, 2, 21, 17, 44, 30, 1), Time.time_with_datetime_fallback(:utc, 2005, 2, 21, 17, 44, 30, 1) #with usec
+
     # This won't overflow on 64bit linux
     unless time_is_64bits?
-      assert_equal Time.time_with_datetime_fallback(:local, 1900, 2, 21, 17, 44, 30), DateTime.civil(1900, 2, 21, 17, 44, 30, DateTime.local_offset, 0)
-      assert_equal Time.time_with_datetime_fallback(:utc, 2039, 2, 21, 17, 44, 30, 1),
-                   DateTime.civil(2039, 2, 21, 17, 44, 30, 0, 0)
+      assert_equal DateTime.civil(1900, 2, 21, 17, 44, 30, DateTime.local_offset, 0), Time.time_with_datetime_fallback(:local, 1900, 2, 21, 17, 44, 30)
+      assert_equal DateTime.civil(2039, 2, 21, 17, 44, 30, 0, 0), Time.time_with_datetime_fallback(:utc, 2039, 2, 21, 17, 44, 30, 1)
       assert_equal ::Date::ITALY, Time.time_with_datetime_fallback(:utc, 2039, 2, 21, 17, 44, 30, 1).start # use Ruby's default start value
     end
+
     silence_warnings do
       0.upto(138) do |year|
         [:utc, :local].each do |format|
@@ -615,17 +616,17 @@ class TimeExtCalculationsTest < ActiveSupport::TestCase
   end
 
   def test_utc_time
-    assert_equal Time.utc_time(2005, 2, 21, 17, 44, 30), Time.utc(2005, 2, 21, 17, 44, 30)
-    assert_equal Time.utc_time(2039, 2, 21, 17, 44, 30), DateTime.civil(2039, 2, 21, 17, 44, 30, 0, 0)
-    assert_equal Time.utc_time(1901, 2, 21, 17, 44, 30), DateTime.civil(1901, 2, 21, 17, 44, 30, 0, 0)
+    assert_equal Time.utc(2005, 2, 21, 17, 44, 30), Time.utc_time(2005, 2, 21, 17, 44, 30)
+    assert_equal DateTime.civil(2039, 2, 21, 17, 44, 30, 0, 0), Time.utc_time(2039, 2, 21, 17, 44, 30)
+    assert_equal DateTime.civil(1901, 2, 21, 17, 44, 30, 0, 0), Time.utc_time(1901, 2, 21, 17, 44, 30)
   end
 
   def test_local_time
-    assert_equal Time.local_time(2005, 2, 21, 17, 44, 30), Time.local(2005, 2, 21, 17, 44, 30)
-    assert_equal Time.local_time(2039, 2, 21, 17, 44, 30), DateTime.civil(2039, 2, 21, 17, 44, 30, DateTime.local_offset, 0)
+    assert_equal Time.local(2005, 2, 21, 17, 44, 30), Time.local_time(2005, 2, 21, 17, 44, 30)
+    assert_equal DateTime.civil(2039, 2, 21, 17, 44, 30, DateTime.local_offset, 0), Time.local_time(2039, 2, 21, 17, 44, 30)
 
     unless time_is_64bits?
-      assert_equal Time.local_time(1901, 2, 21, 17, 44, 30), DateTime.civil(1901, 2, 21, 17, 44, 30, DateTime.local_offset, 0)
+      assert_equal DateTime.civil(1901, 2, 21, 17, 44, 30, DateTime.local_offset, 0), Time.local_time(1901, 2, 21, 17, 44, 30)
     end
   end
 
