fbebed31c1df9a2907151500e6f389d16d93ab52
Sven Fuchs <svenfuchs@artweb-design.de>
2011-07-11 01:29:22 +0200

    refactor ci/travis/ci.rb

--- ci/travis/ci.rb
+++ ci/travis/ci.rb
@@ -5,124 +5,126 @@ include FileUtils
 commands = [
   'mysql -e "create database activerecord_unittest;"',
   'mysql -e "create database activerecord_unittest2;"',
-  'psql  -c "create database activerecord_unittest;" -U postgres > /dev/null',
-  'psql  -c "create database activerecord_unittest2;" -U postgres > /dev/null'
+  'psql  -c "create database activerecord_unittest;" -U postgres',
+  'psql  -c "create database activerecord_unittest2;" -U postgres'
 ]
 commands.each do |command|
-  system(command)
+  system("#{command} > /dev/null 2>&1")
 end
 
-def rake(*tasks)
-  tasks.each do |task|
-    cmd = "bundle exec rake #{task}"
-    puts "Running command: #{cmd}"
-    return false unless system(cmd)
+class Build
+  attr_reader :component, :options, :results
+
+  def initialize(component, options = {})
+    @component = component
+    @options = options
+    @results = []
   end
-  true
-end
 
-RESULTS  = {}
-DEFAULTS = { :normal => 'test', :isolated => 'test:isolated' }
-
-def gem
-  ENV['GEM'].to_sym
-end
-
-def isolated?
-  ENV['ISOLATED'] == 'true'
-end
-
-def identity_map?(options)
-  ENV['IM'] == 'true'
-end
-
-def announce(section)
-  puts
-  puts "\e[1;33m[Travis CI] #{section}\e[m"
-  puts
-end
-
-def build(gem, options = {})
-  config  = DEFAULTS.merge(options)
-  name    = config[:name] || gem
-  dir     = config[:dir] || gem.to_s
-  command = config[:command] || config[isolated? ? :isolated : :normal]
-
-  cd(dir) do
-    announce name
-    RESULTS[gem] = rake(*Array(command))
-  end if command
-end
-
-def build_active_record(adapter, options = {})
-  if identity_map?(options)
-    ENV['IM'] = 'true'
-    name = "activerecord with #{adapter} IM enabled"
-    key  = :"activerecord_#{adapter}_IM"
-  else
-    ENV['IM'] = 'false'
-    name = "activerecord with #{adapter} IM disabled"
-    key  = :"activerecord_#{adapter}"
+  def run!(options = {})
+    self.options.update(options)
+    cd(dir) do
+      announce(heading)
+      ENV['IM'] = identity_map?.inspect
+      results[component] = rake(*tasks)
+      summary
+    end
   end
-  command = 'mysql:rebuild_databases', "#{adapter}:#{'isolated_' if isolated?}test"
-  build(key, :name => name, :command => command, :dir => 'activerecord')
-end
 
-if gem == :activerecord
-  [:mysql, :mysql2, :sqlite3].each do |adapter|
-    build_active_record(adapter)
+  def announce(heading)
+    puts "\n\e[1;33m[Travis CI] #{heading}\e[m\n"
   end
-else
-  build(gem)
+
+  def heading
+    heading = [gem]
+    heading << "with #{adapter} IM #{identity_map? ? 'enabled' : 'disabled'}" if activerecord?
+    heading << "in isolation" if isolated?
+    heading.join(' ')
+  end
+
+  def summary
+  end
+
+  def failures
+    results.select { |key, value| value == false }
+  end
+
+  def tasks
+    if activerecord?
+      ['mysql:rebuild_databases', "#{adapter}:#{'isolated_' if isolated?}test"]
+    else
+      ["test:#{'isolated_' if isolated?}"]
+    end
+  end
+
+  def key
+    key = [gem]
+    key << adapter if activerecord?
+    key << 'IM' if identity_map?
+    key << 'isolated' if isolated?
+    key.join(':')
+  end
+
+  def activerecord?
+    gem == 'activerecord'
+  end
+
+  def identity_map?
+    options[:identity_map]
+  end
+
+  def isolated?
+    options[:isolated]
+  end
+
+  def gem
+    component.split(':').first
+  end
+  alias :dir :gem
+
+  def adapter
+    component.split(':').last
+  end
+
+  def rake(*tasks)
+    tasks.each do |task|
+      cmd = "bundle exec rake #{task}"
+      puts "Running command: #{cmd}"
+      return false unless system(cmd)
+    end
+    true
+  end
 end
 
+ENV['GEM'].split(',').each do |gem|
+  build = Build.new(gem, :isolated => ENV['ISOLATE'])
+  build.run!
+  build.run!(:identity_map => true) if build.activerecord?
+end
 
+# puts
+# puts "Build environment:"
+# puts "  #{`cat /etc/issue`}"
+# puts "  #{`uname -a`}"
+# puts "  #{`ruby -v`}"
+# puts "  #{`mysql --version`}"
+# # puts "  #{`pg_config --version`}"
+# puts "  SQLite3: #{`sqlite3 -version`}"
+# `gem env`.each_line {|line| print "   #{line}"}
+# puts "   Bundled gems:"
+# `bundle show`.each_line {|line| print "     #{line}"}
+# puts "   Local gems:"
+# `gem list`.each_line {|line| print "     #{line}"}
 
+failures = build.failures
 
-
-# unless identity_map?
-#   build :activesupport
-#   build :railties
-#   build :actionpack
-#   build :actionmailer
-#   build :activemodel
-#   build :activeresource
-# end
-#
-# # We currently have issues with configuring postgres inside vagrant properly so
-# # that it supports the required number of connections for the rails test suite
-# # (which seems to be huge). So we have to leave postgres out until this has been
-# # fixed.
-# #
-# # :postgresql
-#
-# [:mysql, :mysql2, :sqlite3].each do |adapter|
-#   build_active_record adapter
-# end
-
-puts
-puts "Build environment:"
-puts "  #{`cat /etc/issue`}"
-puts "  #{`uname -a`}"
-puts "  #{`ruby -v`}"
-puts "  #{`mysql --version`}"
-# puts "  #{`pg_config --version`}"
-puts "  SQLite3: #{`sqlite3 -version`}"
-`gem env`.each_line {|line| print "   #{line}"}
-puts "   Bundled gems:"
-`bundle show`.each_line {|line| print "     #{line}"}
-puts "   Local gems:"
-`gem list`.each_line {|line| print "     #{line}"}
-
-failures = RESULTS.select { |key, value| value == false }
-
-if failures.empty?
+if build.failures.empty?
   puts
   puts "Rails build finished sucessfully"
   exit(true)
 else
   puts
   puts "Rails build FAILED"
-  puts "Failed components: #{failures.map { |component| component.first }.join(', ')}"
+  puts "Failed components: #{build.failures.map { |component| component.first }.join(', ')}"
   exit(false)
 end
