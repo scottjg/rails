b12358e2fece0a15dc88ca308c3f07ec2d2ebb3e
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-07-14 21:18:25 +0200

    minor change for xavier

--- ci/travis/ci.rb
+++ ci/travis/ci.rb
@@ -8,6 +8,7 @@ commands = [
   'psql  -c "create database activerecord_unittest;" -U postgres',
   'psql  -c "create database activerecord_unittest2;" -U postgres'
 ]
+
 commands.each do |command|
   system("#{command} > /dev/null 2>&1")
 end
