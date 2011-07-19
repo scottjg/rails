d524a894854602be7264720410585d85009c34a6
Jordan Brough <jordan@animoto.com>
2011-07-18 21:06:41 -0600

    unicode patch for Rails escape_javascript
    use actual javascript unicode escapes.
    tested in osx safari 5/ff3.6/chrome9 and winxp ie7/ie8
    per info on:
    https://developer.mozilla.org/en/Core_JavaScript_1.5_Guide/Core_Language_Features#Unicode
    and looking at:
    http://code.google.com/apis/opensocial/docs/0.8/reference/gadgets/#gadgets.util.escapeString

--- actionpack/lib/action_view/helpers/javascript_helper.rb
+++ actionpack/lib/action_view/helpers/javascript_helper.rb
@@ -10,7 +10,9 @@ module ActionView
         "\n"    => '\n',
         "\r"    => '\n',
         '"'     => '\\"',
-        "'"     => "\\'" }
+        "'"     => "\\'",
+        "\342\200\250" => '\u2028', # unicode line separator
+        "\342\200\251" => '\u2029' } # unicode paragraph separator
 
       # Escape carrier returns and single and double quotes for JavaScript segments.
       # Also available through the alias j(). This is particularly helpful in JavaScript responses, like:
@@ -18,7 +20,7 @@ module ActionView
       #   $('some_element').replaceWith('<%=j render 'some/element_template' %>');
       def escape_javascript(javascript)
         if javascript
-          result = javascript.gsub(/(\\|<\/|\r\n|[\n\r"'])/) {|match| JS_ESCAPE_MAP[match] }
+          result = javascript.gsub(/(\\|<\/|\r\n|[\n\r"']|\342\200\250|\342\200\251)/) {|match| JS_ESCAPE_MAP[match] }
           javascript.html_safe? ? result.html_safe : result
         else
           ''
