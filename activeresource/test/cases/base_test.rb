fc6b920ced1aac05acc669fe858d3d40d1e66848
John Mileham <jmileham@gmail.com>
2011-08-24 15:18:55 -0400

    Failing test: ActiveResource shouldn't rely on the presence of Content-Length

--- activeresource/test/cases/base_test.rb
+++ activeresource/test/cases/base_test.rb
@@ -636,13 +636,37 @@ class BaseTest < Test::Unit::TestCase
     assert_nil p.__send__(:id_from_response, resp)
   end
 
-  def test_load_attributes_from_response
+  def test_load_attributes_from_response_with_no_body
     p = Person.new
     resp = ActiveResource::Response.new(nil)
     resp['Content-Length'] = "100"
     assert_nil p.__send__(:load_attributes_from_response, resp)
   end
 
+  def test_load_attributes_from_response_with_zero_content_length
+    p = Person.new
+    resp = ActiveResource::Response.new(@rick)
+    resp['Content-Length'] = "0"
+    assert_nil p.__send__(:load_attributes_from_response, resp)
+  end
+
+  # These response codes aren't allowed to have bodies per HTTP spec
+  def test_load_attributes_from_response_with_empty_response_codes
+    p = Person.new
+    [100,101,204,304].each do |status_code|
+      resp = ActiveResource::Response.new(@rick, status_code)
+      assert_nil p.__send__(:load_attributes_from_response, resp)
+    end
+  end
+
+  # Content-Length is not required by HTTP 1.1, so we should read
+  # the body anyway in its absence.
+  def test_load_attributes_from_response_with_no_content_length
+    p = Person.new
+    resp = ActiveResource::Response.new(@rick)
+    resp['Content-Length'] = nil
+    assert_not_nil p.__send__(:load_attributes_from_response, resp)
+  end
 
   def test_create_with_custom_prefix
     matzs_house = StreetAddress.new(:person_id => 1)
