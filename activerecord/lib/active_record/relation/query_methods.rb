580b24cc3231bd955e27f9d0faa88c2127752bf7
Brandon Dewitt <brandon@myjibe.com>
2011-08-31 02:27:09 -0400

    add multiple values to having clauses for query methods

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -183,7 +183,12 @@ module ActiveRecord
         arel = arel.where(Arel::Nodes::Grouping.new(where))
       end
 
-      arel = arel.having(*@having_values.uniq.reject{|h| h.blank?}) unless @having_values.empty?
+      (@having_values - ['']).uniq.each do |having|
+        having = Arel.sql(having) if String === having 
+        arel = arel.having(Arel::Nodes::Grouping.new(having))
+      end 
+
+      #arel = arel.having(*@having_values.uniq.reject{|h| h.blank?}) unless @having_values.empty?
 
       arel = arel.take(@limit_value) if @limit_value
       arel = arel.skip(@offset_value) if @offset_value
