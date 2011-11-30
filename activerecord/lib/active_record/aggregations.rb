8659c92a7938233fb4a4f17e6f1c942746dc5bf6
Patrick Negri <patrick@iugu.com.br>
2011-11-30 12:36:12 -0200

    fixes composed_of for delegated attributes and methods

--- activerecord/lib/active_record/aggregations.rb
+++ activerecord/lib/active_record/aggregations.rb
@@ -222,8 +222,8 @@ module ActiveRecord
       private
         def reader_method(name, class_name, mapping, allow_nil, constructor)
           define_method(name) do
-            if @aggregation_cache[name].nil? && (!allow_nil || mapping.any? {|pair| !read_attribute(pair.first).nil? })
-              attrs = mapping.collect {|pair| read_attribute(pair.first)}
+            if @aggregation_cache[name].nil? && (!allow_nil || mapping.any? {|pair| !self.send(pair.first).nil? })
+              attrs = mapping.collect {|pair| self.send(pair.first)}
               object = constructor.respond_to?(:call) ?
                 constructor.call(*attrs) :
                 class_name.constantize.send(constructor, *attrs)
