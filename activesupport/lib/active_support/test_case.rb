a51d14cdd2b541daa7c39f9759702558b328a89c
Joshua Born <joshua.born@gmail.com>
2012-01-25 12:12:31 -0500

    Comment out load of Mocha from ActiveSupport in order to make Mocha work again

--- activesupport/lib/active_support/test_case.rb
+++ activesupport/lib/active_support/test_case.rb
@@ -5,7 +5,7 @@ require 'active_support/testing/deprecation'
 require 'active_support/testing/declarative'
 require 'active_support/testing/pending'
 require 'active_support/testing/isolation'
-require 'active_support/testing/mochaing'
+#require 'active_support/testing/mochaing'
 require 'active_support/core_ext/kernel/reporting'
 
 module ActiveSupport
