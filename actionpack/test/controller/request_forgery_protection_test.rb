scottjg/rails

commit 16ee611fad37b6b271088eda4cdbe3d6be088af1

Do not include the authenticity token in forms where remote: true as ajax forms use the meta-tag value

actionpack/lib/action_view/helpers/form_tag_helper.rb
@@ -609,8 +609,15 @@ module ActionView
             # responsibility of the caller to escape all the values.
             html_options["action"]  = url_for(url_for_options)
             html_options["accept-charset"] = "UTF-8"
-            html_options["data-remote"] = true if html_options.delete("remote")
-            html_options["authenticity_token"] = html_options.delete("authenticity_token") if html_options.has_key?("authenticity_token")
+            
+            if html_options.delete("remote")
+              html_options["data-remote"] = true 
+
+              # The authenticity token is taken from the meta tag in this case
+              html_options["authenticity_token"] = false
+            else
+              html_options["authenticity_token"] = html_options.delete("authenticity_token") if html_options.has_key?("authenticity_token")
+            end
           end
         end
 

actionpack/test/controller/request_forgery_protection_test.rb
@@ -37,6 +37,10 @@ module RequestForgeryProtectionActions
     render :inline => "<%= form_for(:some_resource, :authenticity_token => false ) {} %>"
   end
 
+  def form_for_remote
+    render :inline => "<%= form_for(:some_resource, :remote => true ) {} %>"
+  end
+
   def rescue_action(e) raise e end
 end
 
@@ -101,6 +105,13 @@ module RequestForgeryProtectionTests
       get :show_button
     end
     assert_select 'form>div>input[name=?][value=?]', 'custom_authenticity_token', @token
+  end
+
+  def test_should_render_form_without_token_tag_if_remote
+    assert_not_blocked do
+      get :form_for_remote
+    end
+    assert_no_match /authenticity_token/, response.body
   end
 
   def test_should_allow_get

commit 3a0c3f34c9e36d7035dfc40e04006b97df0dbca0

Added config.action_view.embed_authenticity_token_in_forms
There is a regression introduced in 16ee611fa, which breaks
remote forms that should also work without javascript. This commit
introduces config option that allows to configure this behavior
defaulting to the old behavior (ie. include authenticity token
in remote forms by default)

actionpack/lib/action_view/helpers/form_tag_helper.rb
@@ -2,6 +2,7 @@ require 'cgi'
 require 'action_view/helpers/tag_helper'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/string/output_safety'
+require 'active_support/core_ext/module/attribute_accessors'
 
 module ActionView
   # = Action View Form Tag Helpers
@@ -17,6 +18,9 @@ module ActionView
       include UrlHelper
       include TextHelper
 
+      mattr_accessor :embed_authenticity_token_in_remote_forms
+      self.embed_authenticity_token_in_remote_forms = true
+
       # Starts a form tag that points the action to an url configured with <tt>url_for_options</tt> just like
       # ActionController::Base#url_for. The method for the form defaults to POST.
       #
@@ -27,9 +31,11 @@ module ActionView
       #   is added to simulate the verb over post.
       # * <tt>:authenticity_token</tt> - Authenticity token to use in the form. Use only if you need to
       #   pass custom authenticity token string, or to not add authenticity_token field at all
-      #   (by passing <tt>false</tt>). If this is a remote form, the authenticity_token will by default
-      #   not be included as the ajax handler will get it from the meta-tag (but you can force it to be
-      #   rendered anyway in that case by passing <tt>true</tt>).
+      #   (by passing <tt>false</tt>).  If this is a remote form and
+      #   <tt>config.action_view.embed_authenticity_token_in_remote_forms</tt> is set to false,
+      #   the authenticity_token will by default not be included as the ajax handler will
+      #   get it from the meta-tag (but you can force it to be rendered anyway in that case
+      #   by passing <tt>true</tt>).
       # * A list of parameters to feed to the URL the form will be posted to.
       # * <tt>:remote</tt> - If set to true, will allow the Unobtrusive JavaScript drivers to control the
       #   submit behavior. By default this behavior is an ajax submit.
@@ -611,16 +617,16 @@ module ActionView
             # responsibility of the caller to escape all the values.
             html_options["action"]  = url_for(url_for_options)
             html_options["accept-charset"] = "UTF-8"
-            
+
             html_options["data-remote"] = true if html_options.delete("remote")
 
-            if html_options["data-remote"] && html_options["authenticity_token"] == true
+            if html_options["data-remote"] && !embed_authenticity_token_in_remote_forms
+              # The authenticity token is taken from the meta tag in this case
+              html_options["authenticity_token"] = false
+            elsif html_options["authenticity_token"] == true
               # Include the default authenticity_token, which is only generated when its set to nil,
               # but we needed the true value to override the default of no authenticity_token on data-remote.
               html_options["authenticity_token"] = nil
-            elsif html_options["data-remote"]
-              # The authenticity token is taken from the meta tag in this case
-              html_options["authenticity_token"] = false
             end
           end
         end

actionpack/lib/action_view/railtie.rb
@@ -7,6 +7,16 @@ module ActionView
     config.action_view = ActiveSupport::OrderedOptions.new
     config.action_view.stylesheet_expansions = {}
     config.action_view.javascript_expansions = { :defaults => %w(jquery jquery_ujs) }
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    initializer "action_view.prepend_authenticity_token_in_forms" do |app|
+      unless app.config.cache_classes
+        ActiveSupport.on_load(:action_view) do
+          ActionView::Helpers::FormTagHelper.embed_authenticity_token_in_remote_forms =
+            app.config.action_view.embed_authenticity_token_in_remote_forms
+        end
+      end
+    end
 
     initializer "action_view.cache_asset_ids" do |app|
       unless app.config.cache_classes
@@ -31,7 +41,7 @@ module ActionView
     initializer "action_view.set_configs" do |app|
       ActiveSupport.on_load(:action_view) do
         app.config.action_view.each do |k,v|
-          send "#{k}=", v
+          send "#{k}=", v if respond_to?("#{k}=")
         end
       end
     end

railties/guides/source/configuring.textile
@@ -375,6 +375,8 @@ And can reference in the view with the following code:
 
 * +config.action_view.cache_asset_ids+ With the cache enabled, the asset tag helper methods will make fewer expensive file system calls (the default implementation checks the file system timestamp). However this prevents you from modifying any asset files while the server is running.
 
+* +config.action_view.embed_authenticity_token_in_remote_forms+ This is by default set to true. If you set it to false, authenticity_token will not be added to forms with +:remote => true+ by default. You can force +authenticity_token+ to be added to such remote form by passing +:authenticity_token => true+ option.
+
 h4. Configuring Action Mailer
 
 There are a number of settings available on +config.action_mailer+:

actionpack/test/controller/request_forgery_protection_test.rb
@@ -45,6 +45,14 @@ module RequestForgeryProtectionActions
     render :inline => "<%= form_for(:some_resource, :remote => true, :authenticity_token => true ) {} %>"
   end
 
+  def form_for_with_token
+    render :inline => "<%= form_for(:some_resource, :authenticity_token => true ) {} %>"
+  end
+
+  def form_for_remote_with_external_token
+    render :inline => "<%= form_for(:some_resource, :remote => true, :authenticity_token => 'external_token') {} %>"
+  end
+
   def rescue_action(e) raise e end
 end
 
@@ -111,16 +119,42 @@ module RequestForgeryProtectionTests
     assert_select 'form>div>input[name=?][value=?]', 'custom_authenticity_token', @token
   end
 
-  def test_should_render_form_without_token_tag_if_remote
+  def test_should_render_form_with_token_tag_if_remote
     assert_not_blocked do
       get :form_for_remote
     end
-    assert_no_match(/authenticity_token/, response.body)
+    assert_match(/authenticity_token/, response.body)
+  end
+
+  def test_should_render_form_without_token_tag_if_remote_and_embedding_token_is_off
+    begin
+      ActionView::Helpers::FormTagHelper.embed_authenticity_token_in_remote_forms = false
+      assert_not_blocked do
+        get :form_for_remote
+      end
+      assert_no_match(/authenticity_token/, response.body)
+    ensure
+      ActionView::Helpers::FormTagHelper.embed_authenticity_token_in_remote_forms = true
+    end
+  end
+
+  def test_should_render_form_with_token_tag_if_remote_and_external_authenticity_token_requested
+    assert_not_blocked do
+      get :form_for_remote_with_external_token
+    end
+    assert_select 'form>div>input[name=?][value=?]', 'custom_authenticity_token', 'external_token'
   end
 
   def test_should_render_form_with_token_tag_if_remote_and_authenticity_token_requested
     assert_not_blocked do
       get :form_for_remote_with_token
+    end
+    assert_select 'form>div>input[name=?][value=?]', 'custom_authenticity_token', @token
+  end
+
+  def test_should_render_form_with_token_tag_with_authenticity_token_requested
+    assert_not_blocked do
+      get :form_for_with_token
     end
     assert_select 'form>div>input[name=?][value=?]', 'custom_authenticity_token', @token
   end
@@ -269,10 +303,6 @@ class FreeCookieControllerTest < ActionController::TestCase
     assert_blank @response.body
   end
 end
-
-
-
-
 
 class CustomAuthenticityParamControllerTest < ActionController::TestCase
   def setup