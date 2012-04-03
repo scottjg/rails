0929650af74ef4117be9414ea42f076cfcb1fa27
kennyj <kennyj@gmail.com>
2012-04-04 00:45:48 +0900

    Fix #5563. Should reflect the most recent change to either of association / id.

--- activerecord/lib/active_record/associations/singular_association.rb
+++ activerecord/lib/active_record/associations/singular_association.rb
@@ -43,7 +43,7 @@ module ActiveRecord
       # ActiveRecord::RecordNotFound is rescued within the method, and it is
       # not reraised. The proxy is \reset and +nil+ is the return value.
       def load_target
-        @target ||= find_target if find_target?
+        @target = find_target if (stale_target? && @stale_state.present?) || find_target?
         loaded! unless loaded?
         target
       rescue ActiveRecord::RecordNotFound
