fbce8c1a095349c82c90a0a2ce84d4f48c377c0b
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-05-04 00:10:25 -0500

    Bring back IdentityMap. This reverts a8dd21d8b459

--- activerecord/lib/active_record.rb
+++ activerecord/lib/active_record.rb
@@ -62,6 +62,7 @@ module ActiveRecord
     autoload :ConnectionHandling
     autoload :DynamicMatchers
     autoload :Explain
+    autoload :IdentityMap
     autoload :Inheritance
     autoload :Integration
     autoload :Migration
