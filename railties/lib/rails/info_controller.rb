02dcb63951e608829320b610373117b8b7bd8d25
schneems <richard.schneeman@gmail.com>
2012-05-22 16:55:06 -0500

    add /rails/info/routes as available domain
    will show similar contents to the output of `$ rake routes` in the browser. This speeds the time required to generate routes, since the application is already initialized.

--- railties/lib/rails/info_controller.rb
+++ railties/lib/rails/info_controller.rb
@@ -1,11 +1,18 @@
+require 'rails/application/route_inspector'
+
 class Rails::InfoController < ActionController::Base
+  self.view_paths = File.join(File.dirname(__FILE__), 'info_controller')
+
   before_filter :require_local!
 
   def show
     case params[:id].to_sym
     when :properties
-      render :inline => Rails::Info.to_html
+      @info = Rails::Info.to_html
+    when :routes
+      @info = Rails::Application::RoutePresenter.display_routes
     end
+     render "templates/#{params[:id]}", :layout => 'layout'
   end
 
   protected
