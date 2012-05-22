1283534ea99ec3a32f75f1b2a330b20616c16fef
schneems <richard.schneeman@gmail.com>
2012-05-22 16:55:29 -0500

    rails info controller tests passing

--- railties/test/rails_info_controller_test.rb
+++ railties/test/rails_info_controller_test.rb
@@ -11,10 +11,10 @@ class InfoControllerTest < ActionController::TestCase
 
   def setup
     Rails.application.routes.draw do
-      get '/rails/info/properties' => "rails/info#properties"
+      get '/rails/info/:id' => "rails/info#show"
     end
     @request.stubs(:local? => true)
-    @controller.stubs(:consider_all_requests_local? => false)
+    Rails.application.config.stubs(:consider_all_requests_local => false)
     @routes = Rails.application.routes
 
     Rails::InfoController.send(:include, @routes.url_helpers)
@@ -22,30 +22,36 @@ class InfoControllerTest < ActionController::TestCase
 
   test "info controller does not allow remote requests" do
     @request.stubs(:local? => false)
-    get :properties
+    get :show, :id => :properties
     assert_response :forbidden
   end
 
   test "info controller renders an error message when request was forbidden" do
     @request.stubs(:local? => false)
-    get :properties
+    get :show, :id => :properties
     assert_select 'p'
   end
 
   test "info controller allows requests when all requests are considered local" do
     @request.stubs(:local? => false)
-    @controller.stubs(:consider_all_requests_local? => true)
-    get :properties
+    Rails.application.config.stubs(:consider_all_requests_local => true)
+    get :show, :id => :properties
     assert_response :success
   end
 
   test "info controller allows local requests" do
-    get :properties
+    get :show, :id => :properties
     assert_response :success
   end
 
   test "info controller renders a table with properties" do
-    get :properties
+    get :show, :id => :properties
     assert_select 'table'
   end
+
+  test "info controller renders with routes" do
+    get :show, :id => :routes
+    assert_select 'pre'
+  end
+
 end
