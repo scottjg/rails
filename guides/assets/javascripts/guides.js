7f1e6a0b7dab0f5f5b734fadf123f442f8c781de
Joe Fiorini <joe@joefiorini.com>
2012-05-26 00:28:24 -0400

    [Guides] Navigation styling for small devices

--- guides/assets/javascripts/guides.js
+++ guides/assets/javascripts/guides.js
@@ -5,3 +5,22 @@ function guideMenu(){
     document.getElementById('guides').style.display = "none";
   }
 }
+
+$.fn.selectGuide = function(guide){
+  $("select", this).val(guide);
+}
+
+guidesIndex = {
+  bind: function(){
+    var currentGuidePath = window.location.pathname;
+    var currentGuide = currentGuidePath.substring(currentGuidePath.lastIndexOf("/")+1);
+    $(".guidesIndex").
+      on("change", "select", guidesIndex.navigate).
+      selectGuide(currentGuide);
+  },
+  navigate: function(e){
+    var $list = $(e.target);
+    url = $list.val();
+    window.location = url;
+  }
+}
