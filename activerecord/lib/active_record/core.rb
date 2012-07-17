795abef2c850c3c0424d970bb8422a448a58d2b4
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-07-17 02:34:57 -0500

    Remove mass assignment security from ActiveRecord

--- activerecord/lib/active_record/core.rb
+++ activerecord/lib/active_record/core.rb
@@ -389,7 +389,6 @@ module ActiveRecord
       @destroyed               = false
       @marked_for_destruction  = false
       @new_record              = true
-      @mass_assignment_options = nil
     end
   end
 end
