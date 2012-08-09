33a20b07718e807b77428911f538cf75eee92a36
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-08-09 19:22:38 -0300

    Test routing concerns with nested resources

--- actionpack/test/dispatch/routing/concerns_test.rb
+++ actionpack/test/dispatch/routing/concerns_test.rb
@@ -13,8 +13,13 @@ class RoutingConcernsTest < ActionDispatch::IntegrationTest
         resources :comments
       end
 
-      resources :posts, concerns: :commentable
-      resource  :picture,  concerns: :commentable
+      resources :posts, concerns: :commentable do
+        resource :video, concerns: :commentable
+      end
+
+      resource :picture, concerns: :commentable do
+        resources :posts, concerns: :commentable
+      end
     end
   end
 
@@ -31,5 +36,17 @@ class RoutingConcernsTest < ActionDispatch::IntegrationTest
     get "/picture/comments"
     assert_equal "200", @response.code
     assert_equal "/picture/comments", picture_comments_path
+  end
+
+  test "accessing concern from nested resource" do
+    get "/posts/1/video/comments"
+    assert_equal "200", @response.code
+    assert_equal "/posts/1/video/comments", post_video_comments_path(post_id: 1)
+  end
+
+  test "accessing concern from nested resources" do
+    get "/picture/posts/1/comments"
+    assert_equal "200", @response.code
+    assert_equal "/picture/posts/1/comments", picture_post_comments_path(post_id: 1)
   end
 end
