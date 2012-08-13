87455940a7bc7f246489fc96635ab99d5aa9d44b
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-08-13 19:02:00 -0300

    Extract common controllers to abstract_unit

--- actionpack/test/dispatch/routing/concerns_test.rb
+++ actionpack/test/dispatch/routing/concerns_test.rb
@@ -1,17 +1,5 @@
 require 'abstract_unit'
 
-class CommentsController < ActionController::Base
-  def index
-    head :ok
-  end
-end
-
-class ImageAttachmentsController < ActionController::Base
-  def index
-    head :ok
-  end
-end
-
 class RoutingConcernsTest < ActionDispatch::IntegrationTest
   Routes = ActionDispatch::Routing::RouteSet.new.tap do |app|
     app.draw do
@@ -20,7 +8,7 @@ class RoutingConcernsTest < ActionDispatch::IntegrationTest
       end
 
       concern :image_attachable do
-        resources :image_attachments, only: :index
+        resources :images, only: :index
       end
 
       resources :posts, concerns: [:commentable, :image_attachable] do
@@ -65,13 +53,13 @@ class RoutingConcernsTest < ActionDispatch::IntegrationTest
   end
 
   test "accessing concern from resources with more than one concern" do
-    get "/posts/1/image_attachments"
+    get "/posts/1/images"
     assert_equal "200", @response.code
-    assert_equal "/posts/1/image_attachments", post_image_attachments_path(post_id: 1)
+    assert_equal "/posts/1/images", post_images_path(post_id: 1)
   end
 
   test "accessing concern from resources using only option" do
-    get "/posts/1/image_attachment/1"
+    get "/posts/1/image/1"
     assert_equal "404", @response.code
   end
 
