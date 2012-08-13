8eabaf2ade1458a731297257fbaf162c656e66e6
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-08-13 16:51:46 +0300

    add -u root to mysql commands as Travis wasn't picking the right user for some reason

--- ci/travis.rb
+++ ci/travis.rb
@@ -3,8 +3,8 @@ require 'fileutils'
 include FileUtils
 
 commands = [
-  'mysql -e "create database activerecord_unittest;"',
-  'mysql -e "create database activerecord_unittest2;"',
+  'mysql -e "create database activerecord_unittest;" -u root',
+  'mysql -e "create database activerecord_unittest2;" -u root',
   'psql  -c "create database activerecord_unittest;" -U postgres',
   'psql  -c "create database activerecord_unittest2;" -U postgres'
 ]
