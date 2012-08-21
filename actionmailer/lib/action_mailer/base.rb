df3252c86b08f7f431f24a4382ef141b144a32ad
Vlad Zloteanu <vlad.zloteanu@dimelo.com>
2012-08-21 17:59:45 +0200

    fix: actionmailer - perform_delivery with smtp

--- actionmailer/lib/action_mailer/base.rb
+++ actionmailer/lib/action_mailer/base.rb
@@ -750,14 +750,19 @@ module ActionMailer #:nodoc:
 
       def perform_delivery_smtp(mail)
         destinations = mail.destinations
+        destinations.map &:strip!
         #mail.ready_to_send
-        sender = (mail['return-path'] && mail['return-path'].spec) || Array(mail.from).first
+        # Set the envelope from to be either the return-path, the sender or the first from address
+        envelope_from = mail.return_path || mail.sender || mail.from_addrs.first
+        if envelope_from.blank?
+          raise ArgumentError.new('A sender (Return-Path, Sender or From) required to send a message')
+        end
 
         smtp = Net::SMTP.new(smtp_settings[:address], smtp_settings[:port])
         smtp.enable_starttls_auto if smtp_settings[:enable_starttls_auto] && smtp.respond_to?(:enable_starttls_auto)
         smtp.start(smtp_settings[:domain], smtp_settings[:user_name], smtp_settings[:password],
                    smtp_settings[:authentication]) do |smtp|
-          smtp.sendmail(mail.encoded, sender, destinations)
+          smtp.sendmail(mail.encoded, envelope_from, destinations)
         end
       end
 
