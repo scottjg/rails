cb79c782f541ffcec7f38500f6eb82b6b3370139
Nathan Broadbent <nathan.f77@gmail.com>
2012-09-28 17:54:27 +1200

    Cherry-picked a0ef2cb from sprockets-rails dont_recompile_unchanged_assets

--- actionpack/lib/sprockets/helpers/rails_helper.rb
+++ actionpack/lib/sprockets/helpers/rails_helper.rb
@@ -10,7 +10,7 @@ module Sprockets
         @asset_paths ||= begin
           paths = RailsHelper::AssetPaths.new(config, controller)
           paths.asset_environment = asset_environment
-          paths.asset_digests     = asset_digests
+          paths.digest_files      = digest_files
           paths.compile_assets    = compile_assets?
           paths.digest_assets     = digest_assets?
           paths
@@ -95,8 +95,8 @@ module Sprockets
         Rails.application.config.assets.prefix
       end
 
-      def asset_digests
-        Rails.application.config.assets.digests
+      def digest_files
+        Rails.application.config.assets.digest_files
       end
 
       def compile_assets?
@@ -115,7 +115,7 @@ module Sprockets
       end
 
       class AssetPaths < ::ActionView::AssetPaths #:nodoc:
-        attr_accessor :asset_environment, :asset_prefix, :asset_digests, :compile_assets, :digest_assets
+        attr_accessor :asset_environment, :asset_prefix, :digest_files, :compile_assets, :digest_assets
 
         class AssetNotPrecompiledError < StandardError; end
 
@@ -129,7 +129,7 @@ module Sprockets
         end
 
         def digest_for(logical_path)
-          if digest_assets && asset_digests && (digest = asset_digests[logical_path])
+          if digest_assets && digest_files && (digest = digest_files[logical_path])
             return digest
           end
 
--- actionpack/lib/sprockets/static_compiler.rb
+++ actionpack/lib/sprockets/static_compiler.rb
@@ -8,23 +8,65 @@ module Sprockets
       @env = env
       @target = target
       @paths = paths
-      @digest = options.key?(:digest) ? options.delete(:digest) : true
-      @manifest = options.key?(:manifest) ? options.delete(:manifest) : true
+      @digest = options.fetch(:digest, true)
+      @manifest = options.fetch(:manifest, true)
       @manifest_path = options.delete(:manifest_path) || target
+      @zip_files = options.delete(:zip_files) || /\.(?:css|html|js|svg|txt|xml)$/
+
+      @current_source_digests = options.fetch(:source_digests, {})
+      @current_digest_files   = options.fetch(:digest_files,   {})
+
+      @digest_files   = {}
+      @source_digests = {}
     end
 
     def compile
-      manifest = {}
+      start_time = Time.now.to_f
+
       env.each_logical_path do |logical_path|
         if File.basename(logical_path)[/[^\.]+/, 0] == 'index'
           logical_path.sub!(/\/index\./, '.')
         end
         next unless compile_path?(logical_path)
-        if asset = env.find_asset(logical_path)
-          manifest[logical_path] = write_asset(asset)
+
+        # Fetch asset without any processing or compression,
+        # to calculate a digest of the concatenated source files
+        asset = env.find_asset(logical_path, :process => false)
+
+        # Force digest to UTF-8, otherwise YAML dumps ASCII-8BIT as !binary
+        @source_digests[logical_path] = asset.digest.force_encoding("UTF-8")
+
+        # Recompile if digest has changed or compiled digest file is missing
+        current_digest_file = @current_digest_files[logical_path]
+
+        if @source_digests[logical_path] != @current_source_digests[logical_path] ||
+           !(current_digest_file && File.exists?("#{@target}/#{current_digest_file}"))
+
+          if asset = env.find_asset(logical_path)
+            @digest_files[logical_path] = write_asset(asset)
+          end
+
+        else
+          # Set asset file from manifest.yml
+          digest_file = @current_digest_files[logical_path]
+          @digest_files[logical_path] = digest_file
+
+          env.logger.debug "Not compiling #{logical_path}, sources digest has not changed " <<
+                           "(#{@source_digests[logical_path][0...7]})"
         end
       end
-      write_manifest(manifest) if @manifest
+
+      if @manifest
+        write_manifest(source_digests: @source_digests, digest_files: @digest_files)
+      end
+
+      # Store digests in Rails config. (Important if non-digest is run after primary)
+      config = ::Rails.application.config
+      config.assets.digest_files   = @digest_files
+      config.assets.source_digests = @source_digests
+
+      elapsed_time = ((Time.now.to_f - start_time) * 1000).to_i
+      env.logger.debug "Processed #{'non-' unless @digest}digest assets in #{elapsed_time}ms"
     end
 
     def write_manifest(manifest)
@@ -43,6 +85,12 @@ module Sprockets
       end
     end
 
+    private
+
+    def path_for(asset)
+      @digest ? asset.digest_path : asset.logical_path
+    end
+
     def compile_path?(logical_path)
       paths.each do |path|
         case path
@@ -55,10 +103,6 @@ module Sprockets
         end
       end
       false
-    end
-
-    def path_for(asset)
-      @digest ? asset.digest_path : asset.logical_path
     end
   end
 end
