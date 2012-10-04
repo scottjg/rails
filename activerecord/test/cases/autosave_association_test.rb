ff7ebb952f3ac8f4fb7b1f99254f2ad9895e4679
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-10-04 10:09:54 -0500

    adding test

--- activerecord/test/cases/autosave_association_test.rb
+++ activerecord/test/cases/autosave_association_test.rb
@@ -1,4 +1,5 @@
 require 'cases/helper'
+require 'models/article'
 require 'models/bird'
 require 'models/company'
 require 'models/customer'
@@ -11,6 +12,7 @@ require 'models/person'
 require 'models/pirate'
 require 'models/post'
 require 'models/reader'
+require 'models/section'
 require 'models/ship'
 require 'models/ship_part'
 require 'models/tag'
@@ -1301,6 +1303,14 @@ class TestAutosaveAssociationOnAHasAndBelongsToManyAssociation < ActiveRecord::T
   end
 
   include AutosaveAssociationOnACollectionAssociationTests
+end
+
+class TestAutosaveInverseAssociationOnAHasManyAssociation < ActiveRecord::TestCase
+  self.use_transactional_fixtures = false
+
+  def test_should_save_the_inverse_association_model
+    Article.create(:sections_attributes => [{ name: 'First' }, { name: 'Last' }])
+  end
 end
 
 class TestAutosaveAssociationValidationsOnAHasManyAssociation < ActiveRecord::TestCase
