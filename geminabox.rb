ac61f1f7b1a12e203c4fb625fcee6f8fe1cae28e
Aaron Quint <aaron@quirkey.com>
2012-10-22 17:42:23 -0400

    Update geminabox script

--- geminabox.rb
+++ geminabox.rb
@@ -1,5 +1,6 @@
 PACKAGES = %w(activesupport activerecord actionpack actionmailer activeresource railties)
 
+build = ENV['PKG_BUILD'] || "pp_#{`git rev-parse HEAD`.strip[0..6]}"
 PACKAGES.each do |p|
-  system "cd #{p} && rm -rf pkg/* && rake package && gem inabox pkg/*.gem && rm -rf pkg/*"
+  system "PKG_BUILD=#{cd #{p} && rm -rf pkg/* && rake package && gem inabox pkg/*.gem && rm -rf pkg/*"
 end
