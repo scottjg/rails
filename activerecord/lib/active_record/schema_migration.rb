94c44d2ed07cbcb0d34cc601d8b460442519bafd
Josh Susser <josh@hasmanythrough.com>
2012-11-25 10:53:37 -0800

    add name, migrated_at to schema_migrations_table

--- activerecord/lib/active_record/schema_migration.rb
+++ activerecord/lib/active_record/schema_migration.rb
@@ -14,9 +14,23 @@ module ActiveRecord
     end
 
     def self.create_table
-      unless connection.table_exists?(table_name)
+      if connection.table_exists?(table_name)
+        cols = connection.columns(table_name).collect { |col| col.name }
+        unless cols.include?("name")
+          connection.add_column table_name, :name, :string, :null => false, :default => ""
+        end
+        unless cols.include?("migrated_at")
+          connection.add_column table_name, :migrated_at, :datetime
+          q_table_name = connection.quote_table_name(table_name)
+          q_timestamp = connection.quoted_date(Time.now)
+          connection.update "UPDATE #{q_table_name} SET migrated_at = '#{q_timestamp}' WHERE migrated_at IS NULL"
+          connection.change_column table_name, :migrated_at, :datetime, :null => false
+        end
+      else
         connection.create_table(table_name, :id => false) do |t|
           t.column :version, :string, :null => false
+          t.column :name, :string, :null => false, :default => ""
+          t.column :migrated_at, :datetime, :null => false
         end
         connection.add_index table_name, :version, :unique => true, :name => index_name
       end
