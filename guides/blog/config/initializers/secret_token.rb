b459d7874ef100b52720c42558371582cf6593e4
Steve Klabnik <steve@steveklabnik.com>
2012-12-19 23:58:00 -0700

    Add dummy blog application

---
+++ guides/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'd79a98077c987e1518709e1fd532481ab5412d63c1f6a6117d2abf0a98eb23b971006909a1cddace1fd6e5b2877d204fb7b4487f10fce5bbe0ad0eeea8f491a2'
