5106da21be06845784b30ace49c195705fcea90d
Abdelkader Boudih <timetravel@pre-history.com>
2013-01-09 12:20:15 +0000

    merging

--- activesupport/lib/active_support/core_ext/infinite_comparable.rb
+++ activesupport/lib/active_support/core_ext/infinite_comparable.rb
@@ -1,30 +1,10 @@
 require 'active_support/concern'
-<<<<<<< HEAD
-require 'active_support/core_ext/object/acts_like'
-=======
 require 'active_support/core_ext/object/try'
->>>>>>> upstream/master
 
 module InfiniteComparable
   extend ActiveSupport::Concern
 
   included do
-<<<<<<< HEAD
-    class_exec do
-      origin_compare = instance_method(:<=>)
-
-      define_method(:<=>) do |other|
-        return origin_compare.bind(self).call(other) if other.class == self.class
-
-        conversion = :"to_#{self.class.name.downcase}"
-        if other.respond_to?(:infinite?) && other.infinite?
-          -other.infinite?
-        elsif other.respond_to?(conversion)
-          origin_compare.bind(self).call(other.send(conversion))
-        else
-          origin_compare.bind(self).call(other)
-        end
-=======
     alias_method_chain :<=>, :infinity
   end
 
@@ -48,7 +28,6 @@ module InfiniteComparable
         conversion = "to_#{self.class.name.downcase}"
         other = other.public_send(conversion) if other.respond_to?(conversion)
         public_send :'<=>_without_infinity', other
->>>>>>> upstream/master
       end
     end
   end
--- activesupport/lib/active_support/core_ext/numeric/infinite_comparable.rb
+++ activesupport/lib/active_support/core_ext/numeric/infinite_comparable.rb
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-require 'active_support/core_ext/big_decimal/conversions'
-require 'active_support/number_helper'
-
-class Numeric
-  [Float, BigDecimal].each do |klass|
-
-    origin_compare = klass.send(:instance_method, :<=>)
-
-    klass.send(:define_method , :<=>) do |other|
-      return origin_compare.bind(self).call(other) if other.class == self.class
-
-      return 0 if infinite? && other.respond_to?(:infinite?) && infinite? == other.infinite?
-
-      infinite? || origin_compare.bind(self).call(other)
-    end
-  end
-=======
 require 'active_support/core_ext/infinite_comparable'
 
 class Float
@@ -24,5 +6,4 @@ end
 
 class BigDecimal
   include InfiniteComparable
->>>>>>> upstream/master
 end
