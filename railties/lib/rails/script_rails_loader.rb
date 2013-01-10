e59bdf569887ed8bab64922b84ba5c3618173f18
Joe Rafaniello <jrafanie@redhat.com>
2013-01-10 19:57:38 +0000

    On an encrypted appliance, script/rails is an executable and cannot be run through ruby. Run script/rails directly on an encrypted appliance and run through ruby otherwise.
    BugzID:15452,16850
    
    git-svn-id: http://miq-ubuntusub.manageiq.com/svn/svnrepos/Manageiq/trunk@41463 3c68ef56-dcc3-11dc-9475-a42b84ecc76f

--- railties/lib/rails/script_rails_loader.rb
+++ railties/lib/rails/script_rails_loader.rb
@@ -2,13 +2,18 @@ require 'pathname'
 
 module Rails
   module ScriptRailsLoader
-    RUBY = File.join(*RbConfig::CONFIG.values_at("bindir", "ruby_install_name")) + RbConfig::CONFIG["EXEEXT"]
     SCRIPT_RAILS = File.join('script', 'rails')
+    # Check for encrypted appliance
+    if File.exists?("/var/www/miq/vmdb") && `file -b #{SCRIPT_RAILS}` =~ /^ELF /
+      RUBY = nil
+    else
+      RUBY = File.join(*RbConfig::CONFIG.values_at("bindir", "ruby_install_name")) + RbConfig::CONFIG["EXEEXT"]
+    end
 
     def self.exec_script_rails!
       cwd = Dir.pwd
       return unless in_rails_application? || in_rails_application_subdirectory?
-      exec RUBY, SCRIPT_RAILS, *ARGV if in_rails_application?
+      exec *([RUBY, SCRIPT_RAILS, *ARGV].compact) if in_rails_application?
       Dir.chdir("..") do
         # Recurse in a chdir block: if the search fails we want to be sure
         # the application is generated in the original working directory.
