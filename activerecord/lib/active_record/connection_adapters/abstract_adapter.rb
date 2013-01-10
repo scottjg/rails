2d185e694ed4de2b5b6b5af83d479715b1812425
Joe Rafaniello <jrafanie@redhat.com>
2013-01-10 19:56:40 +0000

    Modifications made to avoid exposing the contents of SQL to the end user when SQL is invalid.
    Raise generic error message for connection adapter ActiveRecord::StatementInvalid error instead of real error containing SQL query.
    Modified find_by_id_filtered method to raise an error if id argument is not an integer.

--- activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
@@ -51,6 +51,8 @@ module ActiveRecord
       include ActiveSupport::Callbacks
       include MonitorMixin
 
+      MIQ_STATEMENT_INVALID_MESSAGE = "Database statement error encountered"
+
       define_callbacks :checkout, :checkin
 
       attr_accessor :visitor, :pool
@@ -280,7 +282,13 @@ module ActiveRecord
             :binds         => binds) { yield }
         rescue Exception => e
           message = "#{e.class.name}: #{e.message}: #{sql}"
+
           @logger.debug message if @logger
+          $log.error("MIQ(abstract_adapter) Name: [#{name}], Message: [#{message}]") if $log
+
+          # Return a generic message when in production to avoid exposing the contents of the SQL query to an end user
+          message = MIQ_STATEMENT_INVALID_MESSAGE if Rails.env.production?
+
           exception = translate_exception(e, message)
           exception.set_backtrace e.backtrace
           raise exception
