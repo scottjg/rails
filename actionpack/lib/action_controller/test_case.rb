f77a2a6e10e28875df06d3c7e7d7fe3d4eb394c6
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2013-01-21 22:23:35 -0200

    Remove Session::Options in favor of setting the options into env directly
    [Carlos Antonio da Silva + Santiago Pastorino]

--- actionpack/lib/action_controller/test_case.rb
+++ actionpack/lib/action_controller/test_case.rb
@@ -161,7 +161,7 @@ module ActionController
       super
 
       self.session = TestSession.new
-      self.session_options = TestSession::DEFAULT_OPTIONS.merge(:id => SecureRandom.hex(16))
+      self.session_options = TestSession::DEFAULT_OPTIONS
     end
 
     def assign_parameters(routes, controller_path, action, parameters = {})
