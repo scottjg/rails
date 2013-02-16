1c8bac323fa2d01fe10444cd44ba6a24003e7005
Renée De Voursney <renee@nird.us>
2013-02-16 23:50:59 +0200

    try this

--- activerecord/lib/rails/generators/active_record/model/templates/model.rb
+++ activerecord/lib/rails/generators/active_record/model/templates/model.rb
@@ -3,5 +3,8 @@ class <%= class_name %> < <%= parent_class_name.classify %>
 <% attributes.select {|attr| attr.reference? }.each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
 <% end -%>
+<% attributes.select{|at| at.name.end_with?('_id')}.each do |attr| -%>
+  belongs_to :<%= attr.name.split('_id').first.pluralize %>
+<% end -%>
 end
 <% end -%>
