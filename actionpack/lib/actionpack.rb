1761f2d3dad537b433e2f5b533de079fb3ea7e72
Rob Anderson <rob.anderson@paymentcardsolutions.co.uk>
2013-03-19 10:13:13 +0000

    remove annoying deprecation notices

--- actionpack/lib/actionpack.rb
+++ actionpack/lib/actionpack.rb
@@ -1,2 +1,2 @@
 require 'action_pack'
-ActiveSupport::Deprecation.warn 'require "actionpack" is deprecated and will be removed in Rails 3. Use require "action_pack" instead.'
+#ActiveSupport::Deprecation.warn 'require "actionpack" is deprecated and will be removed in Rails 3. Use require "action_pack" instead.'
--- activesupport/lib/activesupport.rb
+++ activesupport/lib/activesupport.rb
@@ -1,2 +1,2 @@
 require 'active_support'
-ActiveSupport::Deprecation.warn 'require "activesupport" is deprecated and will be removed in Rails 3. Use require "active_support" instead.'
+#ActiveSupport::Deprecation.warn 'require "activesupport" is deprecated and will be removed in Rails 3. Use require "active_support" instead.'
