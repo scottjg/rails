ef028c9344c268d270352b7286eef9851f79e782
Sergey Semyonov <slike9@rambler.ru>
2013-04-25 16:42:17 +0400

    (FIX) Error for array of integers for postgres
    Field description in schema: `t.integer :field_name, :type => :array`. 
    Error: "undefined method `gsub' for 1:Fixnum" in PostgreSQLColumn#quote_and_escape.

--- activerecord/lib/active_record/connection_adapters/postgresql/cast.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/cast.rb
@@ -69,14 +69,15 @@ module ActiveRecord
 
         def array_to_string(value, column, adapter, should_be_quoted = false)
           casted_values = value.map do |val|
-            if String === val
-              if val == "NULL"
-                "\"#{val}\""
-              else
-                quote_and_escape(adapter.type_cast(val, column, true))
-              end
+            if val == "NULL"
+              "\"#{val}\""
             else
-              adapter.type_cast(val, column, true)
+              casted_val = adapter.type_cast(val, column, true)
+              if String == casted_val
+                quote_and_escape(casted_val)
+              else
+                casted_val
+              end
             end
           end
           "{#{casted_values.join(',')}}"
