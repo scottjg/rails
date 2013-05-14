d4ec44883ca3edc2d455d2b3dbc9927f2ca2b40e
Sean Wolfe <swolfe@yammer-inc.com>
2013-05-14 12:04:04 -0700

    change require 'config' to require 'rbconfig' to support Ruby 1.9

--- activemodel/test/cases/helper.rb
+++ activemodel/test/cases/helper.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../../../load_paths', __FILE__)
 lib = File.expand_path("#{File.dirname(__FILE__)}/../../lib")
 $:.unshift(lib) unless $:.include?('lib') || $:.include?(lib)
 
-require 'config'
+require 'rbconfig'
 require 'active_model'
 require 'active_support/core_ext/string/access'
 
--- activerecord/test/cases/helper.rb
+++ activerecord/test/cases/helper.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../../../load_paths', __FILE__)
 
-require 'config'
+require 'rbconfig'
 
 require 'test/unit'
 require 'stringio'
