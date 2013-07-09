ce235e7e37f2e2a46ed404beb08c9254f8d885f6
Timm <kaspth@gmail.com>
2013-07-09 19:27:24 +0200

    Changed the implementation of assert_dom_equal.

--- actionpack/lib/action_dispatch/testing/assertions/dom.rb
+++ actionpack/lib/action_dispatch/testing/assertions/dom.rb
@@ -8,9 +8,20 @@ module ActionDispatch
       #   # assert that the referenced method generates the appropriate HTML string
       #   assert_dom_equal '<a href="http://www.example.com">Apples</a>', link_to("Apples", "http://www.example.com")
       def assert_dom_equal(expected, actual, message = "")
-        expected_dom = Loofah.fragment(expected).to_s
-        actual_dom   = Loofah.fragment(actual).to_s
-        assert_equal expected_dom, actual_dom
+        expected_dom = Loofah.fragment(expected)
+        actual_dom   = Loofah.fragment(actual)
+
+        equivalent = true
+        expected_dom.children.each_with_index do |child, i|
+          expected = child.attribute_nodes.sort_by { |a| a.name }
+          actual = actual_dom.children[i].attribute_nodes.sort_by { |a| a.name
+
+          expected.each_with_index do |attr, idx|
+            equivalent &&= attr.name == actual[idx].name  &&
+                            attr.value == actual[idx].value
+          end
+        end
+        assert equivalent
       end
 
       # The negated form of +assert_dom_equivalent+.
