89948d3d951d25e980a5d41838020b0a3df13b65
Timm <kaspth@gmail.com>
2013-07-09 19:52:18 +0200

    Restructured the implementation of assert_dom_equal and assert_dom_not_equal.

--- actionpack/lib/action_dispatch/testing/assertions/dom.rb
+++ actionpack/lib/action_dispatch/testing/assertions/dom.rb
@@ -8,20 +8,7 @@ module ActionDispatch
       #   # assert that the referenced method generates the appropriate HTML string
       #   assert_dom_equal '<a href="http://www.example.com">Apples</a>', link_to("Apples", "http://www.example.com")
       def assert_dom_equal(expected, actual, message = "")
-        expected_dom = Loofah.fragment(expected)
-        actual_dom   = Loofah.fragment(actual)
-
-        equivalent = true
-        expected_dom.children.each_with_index do |child, i|
-          expected = child.attribute_nodes.sort_by { |a| a.name }
-          actual = actual_dom.children[i].attribute_nodes.sort_by { |a| a.name
-
-          expected.each_with_index do |attr, idx|
-            equivalent &&= attr.name == actual[idx].name  &&
-                            attr.value == actual[idx].value
-          end
-        end
-        assert equivalent
+        assert compare_doms_from_strings(expected, actual)
       end
 
       # The negated form of +assert_dom_equivalent+.
@@ -29,10 +16,29 @@ module ActionDispatch
       #   # assert that the referenced method does not generate the specified HTML string
       #   assert_dom_not_equal '<a href="http://www.example.com">Apples</a>', link_to("Oranges", "http://www.example.com")
       def assert_dom_not_equal(expected, actual, message = "")
-        expected_dom = Loofah.fragment(expected).to_s
-        actual_dom   = Loofah.fragment(actual).to_s
-        assert_not_equal expected_dom, actual_dom
+        assert_not_equal compare_doms_from_strings(expected, actual)
       end
+
+      protected
+        def compare_doms_from_strings(expected, actual)
+          expected_dom = Loofah.fragment(expected)
+          actual_dom   = Loofah.fragment(actual)
+
+          expected_dom.children.each_with_index do |child, i|
+            return false unless attributes_are_equal?(child, actual_dom.children[i])
+          end
+          true
+        end
+
+        def attributes_are_equal?(element, other_element)
+          expected = element.attribute_nodes.sort_by { |a| a.name }
+          actual = other_element.attribute_nodes.sort_by { |a| a.name }
+
+          expected.each_with_index do |attr, i|
+            return false unless attr.name == actual[i].name && attr.value == actual[i].value
+          end
+          true
+        end
     end
   end
 end
