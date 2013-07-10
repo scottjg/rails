2dba9a83b5c1217217bdb6e1600b9baedae4f834
Timm <kaspth@gmail.com>
2013-07-10 12:53:46 +0200

    Changed the dom.rb to actually compare elements! Extracted more things and added documentation for new methods.

--- actionpack/lib/action_dispatch/testing/assertions/dom.rb
+++ actionpack/lib/action_dispatch/testing/assertions/dom.rb
@@ -3,41 +3,71 @@ require 'loofah'
 module ActionDispatch
   module Assertions
     module DomAssertions
-      # \Test two HTML strings for equivalency (e.g., identical up to reordering of attributes)
+      # \Test two HTML strings for equivalency (e.g., equal even when attributes are in another order)
       #
       #   # assert that the referenced method generates the appropriate HTML string
       #   assert_dom_equal '<a href="http://www.example.com">Apples</a>', link_to("Apples", "http://www.example.com")
-      def assert_dom_equal(expected, actual, message = "")
-        assert compare_doms_from_strings(expected, actual)
+      def assert_dom_equal(expected, actual, message = nil)
+        expected_dom, actual_dom = doms_from_strings(expected, actual)
+        message ||= "Expected: #{expected_dom}\nActual: #{actual_dom}"
+        assert compare_doms(expected_dom, actual_dom), message
       end
 
-      # The negated form of +assert_dom_equivalent+.
+      # The negated form of +assert_dom_equal+.
       #
       #   # assert that the referenced method does not generate the specified HTML string
       #   assert_dom_not_equal '<a href="http://www.example.com">Apples</a>', link_to("Oranges", "http://www.example.com")
-      def assert_dom_not_equal(expected, actual, message = "")
-        assert_not_equal compare_doms_from_strings(expected, actual)
+      def assert_dom_not_equal(expected, actual, message = nil)
+        expected_dom, actual_dom = doms_from_strings(expected, actual)
+        message ||= "Expected: #{expected_dom}\nActual: #{actual_dom}"
+        assert_not compare_doms(expected_dom, actual_dom), message
       end
 
       protected
-        def compare_doms_from_strings(expected, actual)
-          expected_dom = Loofah.fragment(expected)
-          actual_dom   = Loofah.fragment(actual)
+        # +doms_from_strings+ creates a Loofah::HTML::DocumentFragment for every string in strings
+        def doms_from_strings(*strings)
+          strings.map { |str| Loofah.fragment(str) }
+        end
 
-          expected_dom.children.each_with_index do |child, i|
-            return false unless attributes_are_equal?(child, actual_dom.children[i])
+        # +compare_doms+ takes two doms loops over all their children and compares each child via +equal_children?+
+        def compare_doms(expected, actual)
+          expected.children.each_with_index do |child, i|
+            return false unless equal_children?(child, actual.children[i])
           end
           true
         end
 
-        def attributes_are_equal?(element, other_element)
-          expected = element.attribute_nodes.sort_by { |a| a.name }
-          actual = other_element.attribute_nodes.sort_by { |a| a.name }
+        # +equal_children?+ compares children according to their type
+        # Determines further comparison via said type
+        # i.e. element node children with equal names has their attributes compared using +attributes_are_equal?+
+        def equal_children?(child, other_child)
+          return false unless child.type == other_child.type
 
-          expected.each_with_index do |attr, i|
-            return false unless attr.name == actual[i].name && attr.value == actual[i].value
+          case child.type
+          when Nokogiri::XML::Node::ELEMENT_NODE
+            child.name == other_child.name && attributes_are_equal?(child, other_child)
+          else
+            child.to_s == other_child.to_s
+          end
+        end
+
+        # +attributes_are_equal?+ sorts elements attributes by name and compares
+        # each attribute by calling +equal_attribute?+
+        # If those are true the attributes are considered equal
+        def attributes_are_equal?(element, other_element)
+          first_nodes = element.attribute_nodes.sort_by { |a| a.name }
+          other_nodes = other_element.attribute_nodes.sort_by { |a| a.name }
+
+          return false unless first_nodes.size == other_nodes.size
+          first_nodes.each_with_index do |attr, i|
+            return false unless equal_attribute?(attr, other_nodes[i])
           end
           true
+        end
+
+        # +equal_attribute?+ compares attributes by their name and value
+        def equal_attribute?(attr, other_attr)
+          attr.name == other_attr.name && attr.value == other_attr.value
         end
     end
   end
