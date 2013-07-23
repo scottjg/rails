cce058dc2d2c3e40faff23123ce0ee0f1ce14140
Timm <kaspth@gmail.com>
2013-07-23 20:43:22 +0200

    Fixed a Nokogiri::CSS::SyntaxError by using its expected format for unicode characters.

--- actionpack/test/controller/request_forgery_protection_test.rb
+++ actionpack/test/controller/request_forgery_protection_test.rb
@@ -293,10 +293,10 @@ class RequestForgeryProtectionControllerUsingResetSessionTest < ActionController
   end
 
   test 'should emit a csrf-param meta tag and a csrf-token meta tag' do
-    SecureRandom.stubs(:base64).returns(@token + '<=?')
+    SecureRandom.stubs(:base64).returns(@token + 'U+003c=U+0022U+003fU+0022') # '<="?"'
     get :meta
     assert_select 'meta[name=?][content=?]', 'csrf-param', 'custom_authenticity_token'
-    assert_select 'meta[name=?][content=?]', 'csrf-token', 'cf50faa3fe97702ca1ae&lt;=?'
+    assert_select 'meta[name=?][content=?]', 'csrf-token', 'cf50faa3fe97702ca1aeU+003c=U+0022U+003fU+0022'
   end
 end
 
@@ -306,7 +306,7 @@ class NullSessionDummyKeyGenerator
   end
 end
 
-class RequestForgeryProtectionControllerUsingNullSessionTest < ActionController::TestCase  
+class RequestForgeryProtectionControllerUsingNullSessionTest < ActionController::TestCase
   def setup
     @request.env[ActionDispatch::Cookies::GENERATOR_KEY] = NullSessionDummyKeyGenerator.new
   end
