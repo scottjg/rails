e9400473ec98d47f11d6b5490547dd05a5f19247
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-08-30 11:02:22 +0530

    Use already defined indent method instead of indenting inline

--- railties/lib/rails/generators/rails/controller/controller_generator.rb
+++ railties/lib/rails/generators/rails/controller/controller_generator.rb
@@ -32,23 +32,23 @@ module Rails
           # namespace :foo do
           #   namespace :bar do
           namespace_ladder = class_path.each_with_index.map do |ns, i|
-            %{#{" " * i * 2}namespace :#{ns} do\n  }
+            indent("namespace :#{ns} do\n", i * 2)
           end.join
 
           # Create route
-          #     get "baz/index"
-          route = %{#{" " * depth * 2}get "#{file_name}/#{action}"\n}
+          #      get "baz/index"
+          route = indent("get #{file_name}/#{action}\n", depth * 2)
 
           # Create `end` ladder
           #   end
           # end
           end_ladder = (1..depth).reverse_each.map do |i|
-            "#{" " * i * 2}end\n"
+            indent("end\n", i * 2)
           end.join
 
           # Combine the 3 parts to generate complete route entry
           namespace_ladder + route + end_ladder
         end
+      end
     end
-  end
 end
