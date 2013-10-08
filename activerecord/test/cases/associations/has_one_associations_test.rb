2a7c4b8e2d6b74fc8c2737197a472fda045cefa6
Michael Lavrisha <michael.lavrisha@gmail.com>
2013-10-08 15:19:30 -0600

    Fix #7191. Remove unnecessary transaction when assigning has_one associations.
    Conflicts:
    
    	activerecord/test/cases/associations/has_one_associations_test.rb

--- activerecord/test/cases/associations/has_one_associations_test.rb
+++ activerecord/test/cases/associations/has_one_associations_test.rb
@@ -465,4 +465,29 @@ class HasOneAssociationsTest < ActiveRecord::TestCase
 
     assert_equal car.id, bulb.attributes_after_initialize['car_id']
   end
+
+  def test_building_has_one_association_with_dependent_restrict
+    option_before = ActiveRecord::Base.dependent_restrict_raises
+    ActiveRecord::Base.dependent_restrict_raises = true
+
+    klass = Class.new(ActiveRecord::Base)
+
+    assert_deprecated     { klass.has_one :account, :dependent => :restrict }
+    assert_not_deprecated { klass.has_one :account }
+  ensure
+    ActiveRecord::Base.dependent_restrict_raises = option_before
+  end
+
+  def test_has_one_transaction
+    company = companies(:first_firm)
+    account = Account.find(1)
+
+    company.account # force loading
+    assert_no_queries { company.account = account }
+
+    company.account = nil
+    assert_no_queries { company.account = nil }
+    account = Account.find(2)
+    assert_queries { company.account = account }
+  end
 end
