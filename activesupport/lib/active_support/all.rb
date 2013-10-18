d7e9658bb272fef8d614bdb8e827489fff80051f
Brent Anderson <me@brentjanderson.com>
2013-10-17 18:49:44 -0600

    Deleted

--- activesupport/lib/active_support/all.rb
+++
@@ -1,4 +0,0 @@
-require 'active_support'
-require 'active_support/deprecation'
-require 'active_support/time'
-require 'active_support/core_ext'
--- railties/lib/rails/commands/plugin.rb
+++
@@ -1,9 +0,0 @@
-if ARGV.first != "new"
-  ARGV[0] = "--help"
-else
-  ARGV.shift
-end
-
-require 'rails/generators'
-require 'rails/generators/rails/plugin/plugin_generator'
-Rails::Generators::PluginGenerator.start
